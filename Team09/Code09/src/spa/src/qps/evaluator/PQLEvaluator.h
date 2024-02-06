53bd510a5e7c80f58831d431a7598fe4ff52cbce
Ezekiel Toh <takufunkai@gmail.com>
2024-02-06 17:10:04 +0800

    Format header file and undo gitignore change

--- Team09/Code09/src/spa/src/qps/evaluator/PQLEvaluator.h
+++ Team09/Code09/src/spa/src/qps/evaluator/PQLEvaluator.h
@@ -6,19 +6,17 @@ typedef std::unordered_map<std::string, std::vector<std::string>> Query;
 typedef std::string QueryEntity;
 typedef int QueryResult;
 
-struct Clause
-{
-	std::string type;
-	QueryEntity firstArg;
-	QueryEntity secondArg;
+struct Clause {
+    std::string type;
+    QueryEntity firstArg;
+    QueryEntity secondArg;
 };
 
-class PQLEvaluator
-{
+class PQLEvaluator {
 public:
-	static QueryResult evaluate(Query);
+    static QueryResult evaluate(Query);
 private:
-	static std::vector<QueryEntity> getQueryEntities(Query);
-	static std::vector<Clause> getQueryClauses(Query);
-	static QueryResult projectClausesOntoEntities(std::vector<QueryEntity>, std::vector<Clause>);
+    static std::vector<QueryEntity> getQueryEntities(Query);
+    static std::vector<Clause> getQueryClauses(Query);
+    static QueryResult projectClausesOntoEntities(std::vector<QueryEntity>, std::vector<Clause>);
 };
