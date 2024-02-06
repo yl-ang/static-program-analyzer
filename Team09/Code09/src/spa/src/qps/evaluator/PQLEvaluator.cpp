c0d5b0d50c8935d487b4374b2f931b444c7fc5b0
Ezekiel Toh <takufunkai@gmail.com>
2024-02-06 17:00:48 +0800

    Fix formatting

--- Team09/Code09/src/spa/src/qps/evaluator/PQLEvaluator.cpp
+++ Team09/Code09/src/spa/src/qps/evaluator/PQLEvaluator.cpp
@@ -1,28 +1,26 @@
 #include "PQLEvaluator.h"
 
-QueryResult PQLEvaluator::evaluate(Query query) 
-{
-	std::vector<QueryEntity> entities = getQueryEntities(query);
-	std::vector<Clause> clauses = getQueryClauses(query);
-	QueryResult qr = projectClausesOntoEntities(entities, clauses);
+QueryResult PQLEvaluator::evaluate(Query query) {
+    std::vector<QueryEntity> entities = getQueryEntities(query);
+    std::vector<Clause> clauses = getQueryClauses(query);
+    QueryResult qr = projectClausesOntoEntities(entities, clauses);
 
-	return qr;
+    return qr;
 }
 
-std::vector<QueryEntity> PQLEvaluator::getQueryEntities(Query)
-{
-	// Extract query entities from Query
-	return std::vector<QueryEntity>();
+std::vector<QueryEntity> PQLEvaluator::getQueryEntities(Query) {
+    // Extract query entities from Query
+    return {};
 }
 
-std::vector<Clause> PQLEvaluator::getQueryClauses(Query)
-{
-	// Extract clauses from Query
-	return std::vector<Clause>();
+std::vector<Clause> PQLEvaluator::getQueryClauses(Query) {
+    // Extract clauses from Query
+
+    return {};
 }
 
-QueryResult PQLEvaluator::projectClausesOntoEntities(std::vector<QueryEntity> entities, std::vector<Clause> clauses)
-{
-	// Evaluate each clause and project the result onto the entities
-	return 0;
+QueryResult PQLEvaluator::projectClausesOntoEntities(
+    std::vector<QueryEntity> entities, std::vector<Clause> clauses) {
+    // Evaluate each clause and project the result onto the entities
+    return 0;
 }
