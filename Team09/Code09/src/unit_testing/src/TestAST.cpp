81e673540991701bc8477fe7a13a1c6946e8b251
juslam19 <e0552250@u.nus.edu>
2024-02-06 11:32:35 +0800

    remove: unit tests

--- Team09/Code09/src/unit_testing/src/TestAST.cpp
+++ Team09/Code09/src/unit_testing/src/TestAST.cpp
@@ -7,15 +7,7 @@ void require(bool b) {
     REQUIRE(b);
 }
 
-TEST_CASE("Right Test") {
+TEST_CASE("Test 1") {
     TNode T;
     require(1 == 1);
-}
-
-// TEST_CASE("Wrong Test") {
-//     require(2 == 1);
-// }
-
-TEST_CASE("Another Right Test") {
-    require(3 == 3);
 }
