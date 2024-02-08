9b82bb287ca19037e1ad7ead32aebda7bdf5c4a0
benluiwj <wenjie.lui@gmail.com>
2024-02-08 23:10:48 +0800

    complete source loader implementation

--- Team09/Code09/src/spa/src/sp/SourceLoader.cpp
+++ Team09/Code09/src/spa/src/sp/SourceLoader.cpp
@@ -5,24 +5,86 @@
 #include <string>
 #include <vector>
 
+#include "Symbols.h"
+
+/*
+Iterate through, character by character.
+When we encounter a symbol/space:
+  add the current word into the result array
+  if its a symbol, we determine if its a double symbol and act accordingly
+  if its a space but we have a word to be added, we add the word and continue
+*/
+
 std::vector<std::string> SourceLoader::LoadSIMPLE(std::string simple_input) {
   std::stringstream stringStream(simple_input);
-  std::vector<std::string> processed_input;
-  std::string line;
-  const std::string emptyString = "";
-  // loop through each line delimited by the new line character
-  while (std::getline(stringStream, line, '\n')) {
-    std::stringstream lineStream(line);
-    std::string word;
-    std::vector<std::string> processed_line;
-    while (lineStream >> word) {
-      if (word != emptyString) {
-        processed_input.push_back(word);
+  std::string word = "";
+  std::vector<std::string> parsedString;
+  while (peekNextChar(stringStream) != EOF) {
+    char currentChar = peekNextChar(stringStream);
+    std::cout << "character is: " << currentChar << "\n";
+
+    // ignore white spaces
+    while (std::isspace(peekNextChar(stringStream))) {
+      // if we currently have a word, we add it
+      if (word.length()) {
+        std::cout << "word is: " << word << "\n";
+        parsedString.push_back(word);
+        word = "";
       }
+      getNextChar(stringStream);
     }
-    processed_input.push_back("\n");
+    /* If the next character is a character symbol, add the current word to the
+    result. Then we attempt to create the symbol string.
+    */
+    if (isCharSymbol(currentChar)) {
+      parsedString.push_back(word);
+      word = "";
+      std::string symbolString = createSymbolString(stringStream);
+      parsedString.push_back(symbolString);
+    } else {
+      word += getNextChar(stringStream);
+    }
   }
-  // there will be an extra new line at the end so we remove that
-  processed_input.pop_back();
-  return processed_input;
+
+  // if we still have something in word, add it to the parsed string
+  // TODO(ben): FIND A WAY TO REMOVE THIS STATEMENT
+  if (word.length()) {
+    parsedString.push_back(word);
+  }
+
+  return parsedString;
+}
+
+bool SourceLoader::isCharSymbol(char character) {
+  return SYMBOLS.find(character) != SYMBOLS.end();
+}
+
+bool SourceLoader::isValidSymbolPair(char key, char value) {
+  return SYMBOL_PAIRS.find(key) != SYMBOL_PAIRS.end() &&
+         SYMBOL_PAIRS[key] == value;
+}
+
+char SourceLoader::peekNextChar(std::stringstream& stream) {
+  return static_cast<char>(stream.peek());
+}
+
+char SourceLoader::getNextChar(std::stringstream& stream) {
+  return static_cast<char>(stream.get());
+}
+
+/*
+A symbol may have a pair (eg. <= is actually a <, = pair). We get the
+first symbol first. Then we need to check whether there is a valid pair
+that contains both the first symbol and the next character in the stream.
+If there is a match, we can add both of them. Otherwise, we add 1 of them.
+*/
+std::string SourceLoader::createSymbolString(std::stringstream& stream) {
+  std::string symbolString = "";
+  char symbolChar = getNextChar(stream);
+  symbolString += symbolChar;
+  char nextChar = peekNextChar(stream);
+  if (isValidSymbolPair(symbolChar, nextChar)) {
+    symbolString += getNextChar(stream);
+  }
+  return symbolString;
 }
--- Team09/Code09/src/unit_testing/src/sp/TestSourceLoader.cpp
+++ Team09/Code09/src/unit_testing/src/sp/TestSourceLoader.cpp
@@ -7,29 +7,37 @@ TEST_CASE("Source Loader Tests") {
   SourceLoader sourceLoader;
 
   SECTION("strips single spaces correctly") {
-    std::vector<std::string> expectedResultList_NormalQuery = {
-        "procedure", "a", "{", "\n", "k", "=", "1;", "\n", "}"};
-    std::string inputString_NormalSimple = "procedure a { \n k = 1; \n }";
-    REQUIRE(expectedResultList_NormalQuery ==
-            sourceLoader.LoadSIMPLE(inputString_NormalSimple));
+    std::vector<std::string> expectedResultList = {"procedure", "a", "{", "k",
+                                                   "=",         "1", ";", "}"};
+    std::string inputString = "procedure a { \n k = 1; \n }";
+    REQUIRE(expectedResultList == sourceLoader.LoadSIMPLE(inputString));
   }
 
   SECTION("strips multiple spaces correctly") {
-    std::vector<std::string> expectedResultList_NormalQuery = {
-        "procedure", "a", "{", "\n", "k", "=", "1;", "\n", "}"};
-    std::string inputString_NormalSimple =
-        "procedure a { \n k       =         1; \n }";
-    REQUIRE(expectedResultList_NormalQuery ==
-            sourceLoader.LoadSIMPLE(inputString_NormalSimple));
+    std::vector<std::string> expectedResultList = {"procedure", "a", "{", "k",
+                                                   "=",         "1", ";", "}"};
+    std::string inputString = "procedure a { \n k       =         1; \n }";
+    REQUIRE(expectedResultList == sourceLoader.LoadSIMPLE(inputString));
   }
 
   SECTION("strips multiple lines correctly") {
-    std::vector<std::string> expectedResultList_NormalQuery = {
-        "procedure", "a", "{", "\n", "k",  "=", "1;",
-        "\n",        "a", "=", "2;", "\n", "}"};
-    std::string inputString_NormalSimple =
-        "procedure a { \n k = 1; \n a = 2; \n  }";
-    REQUIRE(expectedResultList_NormalQuery ==
-            sourceLoader.LoadSIMPLE(inputString_NormalSimple));
+    std::vector<std::string> expectedResultList = {
+        "procedure", "a", "{", "k", "=", "1", ";", "a", "=", "2", ";", "}"};
+    std::string inputString = "procedure a { \n k = 1; \n a = 2; \n  }";
+    REQUIRE(expectedResultList == sourceLoader.LoadSIMPLE(inputString));
+  }
+
+  SECTION("loads single line procedure correctly") {
+    std::vector<std::string> expectedResultList = {"procedure", "a", "{", "k",
+                                                   "=",         "1", ";", "}"};
+    std::string inputString = "procedure a {  k = 1; }";
+    REQUIRE(expectedResultList == sourceLoader.LoadSIMPLE(inputString));
+  }
+
+  SECTION("loads un-spaced operations correctly") {
+    std::vector<std::string> expectedResultList = {"procedure", "a", "{", "k",
+                                                   "=",         "1", ";", "}"};
+    std::string inputString = "procedure a {  \n k=1; \n }";
+    REQUIRE(expectedResultList == sourceLoader.LoadSIMPLE(inputString));
   }
 }
