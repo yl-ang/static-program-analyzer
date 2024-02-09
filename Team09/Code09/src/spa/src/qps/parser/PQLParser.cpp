953b58fbba8c32659caf932cab0dc688f6f99e26
juslam19 <e0552250@u.nus.edu>
2024-02-10 01:58:30 +0800

    add: more updated skeleton code

--- Team09/Code09/src/spa/src/qps/parser/PQLParser.cpp
+++ Team09/Code09/src/spa/src/qps/parser/PQLParser.cpp
@@ -1,24 +1,39 @@
 #include "PQLParser.h"
 
 Query PQLParser::parse(UnparsedQuery unparsedQuery) {
-    std::vector<QueryEntity> entities = parseQueryEntities(unparsedQuery);
-    std::vector<Clause> clauses = parseQueryClauses(unparsedQuery);
+    std::string unparsedEntities = getQueryEntities(unparsedQuery);
+    std::string unparsedClauses = getQueryClauses(unparsedQuery);
+    std::vector<QueryEntity> entities = parseQueryEntities(unparsedEntities);
+    std::vector<Clause> clauses = parseQueryClauses(unparsedClauses);
     Query query = combineResult(entities, clauses);
     return query;
 }
 
-std::vector<QueryEntity> PQLParser::parseQueryEntities(UnparsedQuery) {
+std::string getQueryEntities(UnparsedQuery unparsedQuery) {
+    return unparsedQuery[0];
+}
+
+std::string getQueryClauses(UnparsedQuery unparsedQuery) {
+    return unparsedQuery[1];
+}
+
+std::vector<QueryEntity> PQLParser::parseQueryEntities(std::string unparsedEntities) {
     // Parse query entities from UnparsedQuery (std::vector<std::string>)
+    // Input should look something like "call cl, c2; assign a1; stmt s1, s2" at this point
+    // Output should look something like ""
     return {};
 }
 
-std::vector<Clause> PQLParser::parseQueryClauses(UnparsedQuery) {
+std::vector<Clause> PQLParser::parseQueryClauses(std::string unparsedClauses) {
     // Parse clauses from UnparsedQuery (std::vector<std::string>)
+    // Input should look something like ""
+    // Output should look something like ""
     return {};
 }
 
 Query PQLParser::combineResult(
     std::vector<QueryEntity> entities, std::vector<Clause> clauses) {
-    // Just combines the two into a vector
+    // Just combines the two (std::vector<QueryEntity>, std::vector<Clause>) 
+    // into a unordered_map {variables, clauses} as keys
     return {};
 }
