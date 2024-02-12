9b3da0270d27dd3b90c2f386b322e9d688c251b2
wonyeji <e0564722@u.nus.edu>
2024-02-12 14:15:33 +0800

    Add Tokenizer

---
+++ Team09/Code09/src/spa/src/sp/tokenizer/Tokenizer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Token.h"
+#include "BasicToken.h"
+#include "LexicalAnalyzer.h"
+#include "../exceptions/SyntaxError.h"
+
+
+class Tokenizer {
+    private:
+        LexicalAnalyzer lexicalAnalyzer;
+        int stmtNum = 0;
+        const int NO_STMT_NUM = -1;
+        int noStmtNumCountdown = 0;
+
+    public:
+        std::vector<Token> tokenize(std::vector<std::string>); 
+        std::vector<Token> assignTokens(std::vector<BasicToken> input);
+        Token assignKeyword(BasicToken bt);
+        Token assignSymbol(BasicToken bt);
+
+};
