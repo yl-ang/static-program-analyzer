dc9c2213bda79a34308024bbe116d69cd166620f
wonyeji <e0564722@u.nus.edu>
2024-02-12 14:15:52 +0800

    Create Token.h

---
+++ Team09/Code09/src/spa/src/sp/tokenizer/Token.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <string>
+
+enum LEXICAL_TOKEN_TYPE {
+  LETTER,
+  DIGIT,
+  NZDIGIT,
+  PROCEDURE,
+  NAME,
+  INTEGER,
+  WHILE,
+  READ,
+  PRINT,
+  CALL,
+  IF,
+  SEMICOLON,
+  OPEN_CURLY_BRACE,
+  CLOSE_CURLY_BRACE,
+  OPEN_BRACKET,
+  CLOSE_BRACKET,
+  THEN,
+  ELSE,
+  EQUAL,
+  NOT,
+  ANDAND,
+  OR,
+  LESS_THAN,
+  LESS_THAN_OR_EQUAL_TO,
+  GREATER_THAN,
+  GREATER_THAN_OR_EQUAL_TO,
+  EQUAL_CHECK,
+  NOT_EQUAL_CHECK,
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  MOD,
+};
+
+class Token {
+ public:
+  LEXICAL_TOKEN_TYPE type;
+  std::string value;
+  int line_number;
+
+  Token(LEXICAL_TOKEN_TYPE token_type, std::string token_value,
+        int token_line_number) {
+    type = token_type;
+    value = token_value;
+    line_number = token_line_number;
+  }
+};
