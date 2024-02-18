c1257cf28bb01f538d09a44c98d2d78a8b631225
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-02-18 12:13:24 +0800

    Update TestFollowsStore.cpp

--- Team09/Code09/src/unit_testing/src/PKB/Stores/TestFollowsStore.cpp
+++ Team09/Code09/src/unit_testing/src/PKB/Stores/TestFollowsStore.cpp
@@ -1,60 +1,51 @@
 #include "PKB/Stores/FollowsStore.h"
 #include "catch.hpp"
 
-TEST_CASE("FollowsStore - Base Test") {
+TEST_CASE("FollowsStore - All Tests") {
     FollowsStore followsStore;
 
-    SECTION("Test setFollowsStore and getFollower/getFollowee") {
+    SECTION("Test getFollower") {
         followsStore.setFollowsStore({{1, 2}, {3, 4}, {5, 6}});
 
         REQUIRE(followsStore.getFollower(1) == 2);
         REQUIRE(followsStore.getFollower(3) == 4);
         REQUIRE(followsStore.getFollower(5) == 6);
+    }
+
+    SECTION("Test getFollowee") {
+        followsStore.setFollowsStore({{1, 2}, {3, 4}, {5, 6}});
 
         REQUIRE(followsStore.getFollowee(2) == 1);
         REQUIRE(followsStore.getFollowee(4) == 3);
         REQUIRE(followsStore.getFollowee(6) == 5);
     }
 
-    SECTION("Test getFolloweesStar and getFollowersStar") {
+    SECTION("Test getFolloweesStar") {
         followsStore.setFollowsStore({{1, 2}, {2, 3}, {3, 4}});
 
-        REQUIRE(followsStore.getFolloweesStar(1) ==
-                std::unordered_set<StmtNum>{2, 3, 4});
+        REQUIRE(followsStore.getFolloweesStar(1).empty());
         REQUIRE(followsStore.getFolloweesStar(2) ==
-                std::unordered_set<StmtNum>{3, 4});
-        REQUIRE(followsStore.getFolloweesStar(3) ==
-                std::unordered_set<StmtNum>{4});
-        REQUIRE(followsStore.getFolloweesStar(4).empty());
-
-        REQUIRE(followsStore.getFollowersStar(1).empty());
-        REQUIRE(followsStore.getFollowersStar(2) ==
                 std::unordered_set<StmtNum>{1});
-        REQUIRE(followsStore.getFollowersStar(3) ==
+        REQUIRE(followsStore.getFolloweesStar(3) ==
                 std::unordered_set<StmtNum>{1, 2});
-        REQUIRE(followsStore.getFollowersStar(4) ==
+        REQUIRE(followsStore.getFolloweesStar(4) ==
                 std::unordered_set<StmtNum>{1, 2, 3});
     }
 
-    SECTION("Test setFollowsStore with single relationship") {
-        followsStore.setFollowsStore({{1, 2}});
-        REQUIRE(followsStore.getFollower(1) == 2);
-        REQUIRE(followsStore.getFollowee(2) == 1);
-        REQUIRE(followsStore.getFolloweesStar(1) ==
-                std::unordered_set<StmtNum>{2});
-        REQUIRE(followsStore.getFolloweesStar(2).empty());
-        REQUIRE(followsStore.getFollowersStar(1).empty());
+    SECTION("Test getFollowersStar") {
+        followsStore.setFollowsStore({{1, 2}, {2, 3}, {3, 4}});
+
+        REQUIRE(followsStore.getFollowersStar(1) ==
+                std::unordered_set<StmtNum>{2, 3, 4});
         REQUIRE(followsStore.getFollowersStar(2) ==
-                std::unordered_set<StmtNum>{1});
-        REQUIRE(followsStore.containsFollowRelationship(1, 2));
-        REQUIRE_FALSE(followsStore.containsFollowRelationship(2, 1));
-        REQUIRE(followsStore.containsFollowStarRelationship(1, 2));
-        REQUIRE_FALSE(followsStore.containsFollowStarRelationship(2, 1));
+                std::unordered_set<StmtNum>{3, 4});
+        REQUIRE(followsStore.getFollowersStar(3) ==
+                std::unordered_set<StmtNum>{4});
+        REQUIRE(followsStore.getFollowersStar(4).empty());
     }
 
-    SECTION(
-        "Test containsFollowRelationship and containsFollowStarRelationship") {
-        followsStore.setFollowsStore({{1, 2}, {3, 4}, {4, 5}, {5, 6}});
+    SECTION("Test containsFollowRelationship") {
+        followsStore.setFollowsStore({{1, 2}, {3, 4}, {5, 6}});
 
         REQUIRE(followsStore.containsFollowRelationship(1, 2));
         REQUIRE(followsStore.containsFollowRelationship(3, 4));
@@ -63,6 +54,10 @@ TEST_CASE("FollowsStore - Base Test") {
         REQUIRE_FALSE(followsStore.containsFollowRelationship(2, 1));
         REQUIRE_FALSE(followsStore.containsFollowRelationship(4, 3));
         REQUIRE_FALSE(followsStore.containsFollowRelationship(6, 5));
+    }
+
+    SECTION("Test containsFollowStarRelationship") {
+        followsStore.setFollowsStore({{1, 2}, {3, 4}, {4, 5}, {5, 6}});
 
         REQUIRE(followsStore.containsFollowStarRelationship(1, 2));
         REQUIRE(followsStore.containsFollowStarRelationship(3, 5));
