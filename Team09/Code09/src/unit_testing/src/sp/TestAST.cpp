0644b4b89c06a2bb711d1e83d6ed9bc10e6d577a
benluiwj <wenjie.lui@gmail.com>
2024-02-18 18:31:57 +0800

    convert to queue instead

--- Team09/Code09/src/unit_testing/src/sp/TestAST.cpp
+++ Team09/Code09/src/unit_testing/src/sp/TestAST.cpp
@@ -1,5 +1,5 @@
-#include <deque>
 #include <iostream>
+#include <queue>
 
 #include "catch.hpp"
 #include "sp/ast/Ast.h"
@@ -8,34 +8,46 @@
 
 using namespace std;  // NOLINT
 
+std::queue<Token> makeTokenQueue(std::vector<Token> tokens) {
+    std::queue<Token> queue;
+    for (auto token : tokens) {
+        queue.push(token);
+    }
+    return queue;
+}
+
 TEST_CASE("AST Build Tests") {
     AST ast;
 
     SECTION("Build const factor ast correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::INTEGER, "1", 0)};
         ASTNode expectedNode = ASTNode("1", "const");
 
-        ASTNode result = ast.buildFactorAST(inputTokenArray);
+        auto queue = makeTokenQueue(inputTokenArray);
+
+        ASTNode result = ast.buildFactorAST(queue);
         REQUIRE(expectedNode == result);
     }
 
     SECTION("Build variable factor ast correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::NAME, "a", 0)};
         ASTNode expectedNode = ASTNode("a", "var");
+        auto queue = makeTokenQueue(inputTokenArray);
 
-        ASTNode result = ast.buildFactorAST(inputTokenArray);
+        ASTNode result = ast.buildFactorAST(queue);
         REQUIRE(expectedNode == result);
     }
 
     SECTION("Build assignment to const ast correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::NAME, "a", 0),
             Token(LEXICAL_TOKEN_TYPE::EQUAL, "=", 1),
             Token(LEXICAL_TOKEN_TYPE::INTEGER, "1", 0),
             Token(LEXICAL_TOKEN_TYPE::SEMICOLON, ";", 1),
         };
+        auto queue = makeTokenQueue(inputTokenArray);
 
         ASTNode expectedNode = ASTNode("", "assign");
         ASTNode nameNode = ASTNode("a", "var");
@@ -44,12 +56,12 @@ TEST_CASE("AST Build Tests") {
         expectedNode.add_child(nameNode);
         expectedNode.add_child(integerNode);
 
-        ASTNode result = ast.buildAssignmentAST(inputTokenArray);
+        ASTNode result = ast.buildAssignmentAST(queue);
         REQUIRE(expectedNode == result);
     }
 
     SECTION("Build assignment to var ast correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::NAME, "a", 0),
             Token(LEXICAL_TOKEN_TYPE::EQUAL, "=", 1),
             Token(LEXICAL_TOKEN_TYPE::NAME, "b", 0),
@@ -61,13 +73,14 @@ TEST_CASE("AST Build Tests") {
 
         expectedNode.add_child(nameNode);
         expectedNode.add_child(integerNode);
+        auto queue = makeTokenQueue(inputTokenArray);
 
-        ASTNode result = ast.buildAssignmentAST(inputTokenArray);
+        ASTNode result = ast.buildAssignmentAST(queue);
         REQUIRE(expectedNode == result);
     }
 
     SECTION("Build adding two terms correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::NAME, "b", 0),
             Token(LEXICAL_TOKEN_TYPE::ADD, "+", 0),
             Token(LEXICAL_TOKEN_TYPE::NAME, "c", 0)};
@@ -78,13 +91,14 @@ TEST_CASE("AST Build Tests") {
 
         addNode.add_child(constNode);
         addNode.add_child(const2Node);
+        auto queue = makeTokenQueue(inputTokenArray);
 
-        ASTNode result = ast.buildExpressionAST(inputTokenArray);
+        ASTNode result = ast.buildExpressionAST(queue);
         REQUIRE(addNode == result);
     }
 
     SECTION("Build adding multiple terms correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::NAME, "x", 0),
             Token(LEXICAL_TOKEN_TYPE::ADD, "+", 0),
             Token(LEXICAL_TOKEN_TYPE::NAME, "y", 0),
@@ -102,13 +116,14 @@ TEST_CASE("AST Build Tests") {
 
         addNode.add_child(addNode2);
         addNode.add_child(zNode);
+        auto queue = makeTokenQueue(inputTokenArray);
 
-        ASTNode result = ast.buildExpressionAST(inputTokenArray);
+        ASTNode result = ast.buildExpressionAST(queue);
         REQUIRE(addNode == result);
     }
 
     SECTION("Build multiplying two terms correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::NAME, "x", 0),
             Token(LEXICAL_TOKEN_TYPE::MUL, "*", 0),
             Token(LEXICAL_TOKEN_TYPE::NAME, "y", 0)};
@@ -119,13 +134,14 @@ TEST_CASE("AST Build Tests") {
 
         mulNode.add_child(xtNode);
         mulNode.add_child(yNode);
+        auto queue = makeTokenQueue(inputTokenArray);
 
-        ASTNode result = ast.buildTermAST(inputTokenArray);
+        ASTNode result = ast.buildTermAST(queue);
         REQUIRE(mulNode == result);
     }
 
     SECTION("Build add then mul correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::NAME, "x", 0),
             Token(LEXICAL_TOKEN_TYPE::ADD, "+", 0),
             Token(LEXICAL_TOKEN_TYPE::NAME, "y", 0),
@@ -143,13 +159,14 @@ TEST_CASE("AST Build Tests") {
 
         addNode.add_child(xNode);
         addNode.add_child(mulNode);
+        auto queue = makeTokenQueue(inputTokenArray);
 
-        ASTNode result = ast.buildExpressionAST(inputTokenArray);
+        ASTNode result = ast.buildExpressionAST(queue);
         REQUIRE(addNode == result);
     }
 
     SECTION("Build mul then add correctly") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::NAME, "x", 0),
             Token(LEXICAL_TOKEN_TYPE::MUL, "*", 0),
             Token(LEXICAL_TOKEN_TYPE::NAME, "y", 0),
@@ -167,13 +184,14 @@ TEST_CASE("AST Build Tests") {
 
         addNode.add_child(mulNode);
         addNode.add_child(zNode);
+        auto queue = makeTokenQueue(inputTokenArray);
 
-        ASTNode result = ast.buildExpressionAST(inputTokenArray);
+        ASTNode result = ast.buildExpressionAST(queue);
         REQUIRE(addNode == result);
     }
 
     SECTION("Build assignment procedure") {
-        std::deque<Token> inputTokenArray = {
+        std::vector<Token> inputTokenArray = {
             Token(LEXICAL_TOKEN_TYPE::PROCEDURE, "procedure", 0),
             Token(LEXICAL_TOKEN_TYPE::NAME, "a", 0),
             Token(LEXICAL_TOKEN_TYPE::OPEN_CURLY_BRACE, "{", 0),
@@ -195,8 +213,9 @@ TEST_CASE("AST Build Tests") {
 
         stmtList.add_child(equalNode);
         procedure.add_child(stmtList);
+        auto queue = makeTokenQueue(inputTokenArray);
 
-        ASTNode result = ast.buildProcedureAST(inputTokenArray);
+        ASTNode result = ast.buildProcedureAST(queue);
         REQUIRE(procedure == result);
     }
 }
