b392ea3aff3be74d095f069c46f373c7be84955d
benluiwj <wenjie.lui@gmail.com>
2024-02-24 20:22:38 +0800

    remove constantNode class

--- Team09/Code09/src/spa/src/sp/ast/grammar_nodes/ConstantNode.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-#include "sp/ast/AstNode.h"
-class ConstantNode : public ASTNode {
-    explicit ConstantNode(std::string value) : ASTNode(value, "const") {}
-};
