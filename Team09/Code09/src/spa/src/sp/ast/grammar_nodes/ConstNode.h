908b850a2fec050e3756390b28019f5d7a28303f
benluiwj <wenjie.lui@gmail.com>
2024-02-24 20:25:16 +0800

    rename files

--- Team09/Code09/src/spa/src/sp/ast/grammar_nodes/ConstNode.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-#include "sp/ast/AstNode.h"
-#include "sp/ast/grammar_nodes/FactorNode.h"
-class ConstNode : public FactorNode {
-public:
-    explicit ConstNode(std::string value) : FactorNode(value, "const") {}
-};
---
+++ Team09/Code09/src/spa/src/sp/ast/grammar_nodes/ConstantNode.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+#include "sp/ast/AstNode.h"
+#include "sp/ast/grammar_nodes/FactorNode.h"
+class ConstantNode : public FactorNode {
+public:
+    explicit ConstantNode(std::string value) : FactorNode(value, "const") {}
+};
