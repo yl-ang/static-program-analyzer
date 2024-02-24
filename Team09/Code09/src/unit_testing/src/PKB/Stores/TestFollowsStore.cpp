0ed031c8f43feed889baba5ab6e0a5909682d3a0
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-02-24 19:42:05 +0800

    Add hasFollowStarRelationship UTs for ClauseArg
    8 UTs:
    - wildcard, wildcard; wildcard, Integer; Integer, wildcard; Integer, Integer
    - True/False
    
    4 * 2 = 8

--- Team09/Code09/src/unit_testing/src/PKB/Stores/TestFollowsStore.cpp
+++ Team09/Code09/src/unit_testing/src/PKB/Stores/TestFollowsStore.cpp
@@ -154,4 +154,86 @@ TEST_CASE("FollowsStore - All Tests") {
         delete integerArg1;
         delete integerArg2;
     }
+
+    SECTION("Test hasFollowStarRelationship with Wildcards arg1 and arg2, non-empty followerStarMap (Expecting True)") {
+        followsStore.setFollowsStore({{1, 2}, {3, 4}, {5, 6}});
+        ClauseArgument* wildcardArg1 = new Wildcard();
+        ClauseArgument* wildcardArg2 = new Wildcard();
+        REQUIRE(followsStore.hasFollowStarRelationship(*wildcardArg1, *wildcardArg2));
+        delete wildcardArg1;
+        delete wildcardArg2;
+    }
+
+    SECTION("Test hasFollowStarRelationship with Wildcards arg1 and arg2, empty followerStarMap (Expecting False)") {
+        followsStore.setFollowsStore({});
+        ClauseArgument* wildcardArg1 = new Wildcard();
+        ClauseArgument* wildcardArg2 = new Wildcard();
+        REQUIRE_FALSE(followsStore.hasFollowStarRelationship(*wildcardArg1, *wildcardArg2));
+        delete wildcardArg1;
+        delete wildcardArg2;
+    }
+
+    SECTION("Test hasFollowStarRelationship with Wildcard arg1 and Integer arg2 (Expecting True)") {
+        followsStore.setFollowsStore({{1, 2}, {2, 3}, {3, 4}, {5, 6}});
+        // Argument 1 is wildcard
+        // Argument 2 is integer with followeeStars (1, 2, 3)
+        ClauseArgument* wildcardArg1 = new Wildcard();
+        ClauseArgument* integerArg2 = new Integer("4");
+        REQUIRE(followsStore.hasFollowStarRelationship(*wildcardArg1, *integerArg2));
+        delete wildcardArg1;
+        delete integerArg2;
+    }
+
+    SECTION("Test hasFollowStarRelationship with Wildcard arg1 and Integer arg2 (Expecting False)") {
+        followsStore.setFollowsStore({{1, 2}, {2, 3}, {5, 6}});
+        // Argument 1 is wildcard
+        // Argument 2 is integer with no followeeStars
+        ClauseArgument* wildcardArg1 = new Wildcard();
+        ClauseArgument* integerArg2 = new Integer("1");
+        REQUIRE_FALSE(followsStore.hasFollowStarRelationship(*wildcardArg1, *integerArg2));
+        delete wildcardArg1;
+        delete integerArg2;
+    }
+
+    SECTION("Test hasFollowStarRelationship with Integer arg1 and Wildcard arg2 (Expecting True)") {
+        followsStore.setFollowsStore({{1, 2}, {3, 4}, {4, 5}, {5, 6}});
+        // Argument 1 is integer (3) and has followerStars (4, 5, 6)
+        // Argument 2 is wildcard
+        ClauseArgument* integerArg1 = new Integer("3");
+        ClauseArgument* wildcardArg2 = new Wildcard();
+        REQUIRE(followsStore.hasFollowStarRelationship(*integerArg1, *wildcardArg2));
+        delete integerArg1;
+        delete wildcardArg2;
+    }
+
+    SECTION("Test hasFollowStarRelationship with Integer arg1 and Wildcard arg2 (Expecting False)") {
+        followsStore.setFollowsStore({{1, 2}, {2, 3}, {5, 6}});
+        // Test argument 1 is integer, but has no followersStar
+        // Test argument 2 is wildcard
+        ClauseArgument* integerArg1 = new Integer("6");
+        ClauseArgument* wildcardArg2 = new Wildcard();
+        REQUIRE_FALSE(followsStore.hasFollowStarRelationship(*integerArg1, *wildcardArg2));
+        delete integerArg1;
+        delete wildcardArg2;
+    }
+
+    SECTION("Test hasFollowStarRelationship with Integer arg1 and Integer arg2 (Expecting True)") {
+        followsStore.setFollowsStore({{1, 2}, {2, 3}, {3, 4}, {5, 6}});
+        // Test when both arguments are integers and following each other
+        ClauseArgument* integerArg1 = new Integer("1");
+        ClauseArgument* integerArg2 = new Integer("4");
+        REQUIRE(followsStore.hasFollowStarRelationship(*integerArg1, *integerArg2));
+        delete integerArg1;
+        delete integerArg2;
+    }
+
+    SECTION("Test hasFollowStarRelationship with Integer arg1 and Integer arg2 (Expecting False)") {
+        followsStore.setFollowsStore({{1, 2}, {3, 4}, {5, 6}});
+        // Test when both arguments are integers but not following each other
+        ClauseArgument* integerArg1 = new Integer("1");
+        ClauseArgument* integerArg2 = new Integer("6");
+        REQUIRE_FALSE(followsStore.hasFollowStarRelationship(*integerArg1, *integerArg2));
+        delete integerArg1;
+        delete integerArg2;
+    }
 }
