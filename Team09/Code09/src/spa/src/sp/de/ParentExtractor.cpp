19470ad8a4f230ac4ddd5dfb221894c9e0eefd17
wonyeji <e0564722@u.nus.edu>
2024-02-25 17:03:20 +0800

    Update parent logic

--- Team09/Code09/src/spa/src/sp/de/ParentExtractor.cpp
+++ Team09/Code09/src/spa/src/sp/de/ParentExtractor.cpp
@@ -1,22 +1,7 @@
 #include "ParentExtractor.h"
 
+#include <algorithm>
 #include <vector>
-
-void ParentExtractor::visitWhile(WhileNode* node) {
-    int parentStmtNum = node->getStmtNum();
-    std::vector<int> childrenStmtNums = node->getStmtLstNode()->getStmtsStmtNum();
-    for (int i = 0; i < childrenStmtNums.size(); ++i) {
-        this->follows.insert({parentStmtNum, childrenStmtNums[i]});
-    }
-}
-
-void ParentExtractor::visitIf(IfNode* node) {
-    // int parentStmtNum = node->getStmtNum();
-    // std::vector<int> childrenStmtNums = node->getStmtLstNode()->getStmtsStmtNum();
-    // for (int i = 0; i < childrenStmtNums.size(); ++i) {
-    //     this->follows.insert({parentStmtNum, childrenStmtNums[i]});
-    // }
-}
 
 void ParentExtractor::visitProgram(ProgramNode* node) {}
 void ParentExtractor::visitStmtLst(StatementListNode* node) {}
@@ -29,6 +14,26 @@ void ParentExtractor::visitPrint(PrintNode* node) {}
 void ParentExtractor::visitAssign(AssignmentNode* node) {}
 void ParentExtractor::visitVariable(VariableNode* node) {}
 void ParentExtractor::visitConstant(ConstantNode* node) {}
+
+void ParentExtractor::visitWhile(WhileNode* node) {
+    int parentStmtNum = node->getStmtNum();
+    std::vector<int> childrenStmtNums = node->getStmtLstNode()->getStmtsStmtNum();
+    for (int i = 0; i < childrenStmtNums.size(); ++i) {
+        this->follows.insert({parentStmtNum, childrenStmtNums[i]});
+    }
+}
+
+void ParentExtractor::visitIf(IfNode* node) {
+    int parentStmtNum = node->getStmtNum();
+    std::vector<int> thenStmtNums = node->getThenStmtLstNode()->getStmtsStmtNum();
+    std::vector<int> elseStmtNums = node->getElseStmtLstNode()->getStmtsStmtNum();
+    for (int i = 0; i < thenStmtNums.size(); ++i) {
+        this->follows.insert({parentStmtNum, thenStmtNums[i]});
+    }
+    for (int i = 0; i < elseStmtNums.size(); ++i) {
+        this->follows.insert({parentStmtNum, elseStmtNums[i]});
+    }
+}
 
 std::unordered_set<std::pair<StmtNum, StmtNum>> ParentExtractor::getParent() {
     return this->parent;
