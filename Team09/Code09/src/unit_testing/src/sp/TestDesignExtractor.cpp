16203418151c61660ff6b7911ab5b4c3c16929a6
wonyeji <e0564722@u.nus.edu>
2024-02-26 01:02:33 +0800

    Update TestDesignExtractor.cpp

--- Team09/Code09/src/unit_testing/src/sp/TestDesignExtractor.cpp
+++ Team09/Code09/src/unit_testing/src/sp/TestDesignExtractor.cpp
@@ -25,6 +25,47 @@ TEST_CASE("Design Extractor Tests") {
     childrenP.push_back(std::move(zNode));
     auto printNode = std::make_unique<ASTNode>("", "print", std::move(childrenP));
 
+    // while ((c < d) && (e > f)) {
+    //     g = h;
+    // }
+    std::unique_ptr<ASTNode> cNode = std::make_unique<ASTNode>("c", "var");
+    std::unique_ptr<ASTNode> dNode = std::make_unique<ASTNode>("d", "var");
+    std::unique_ptr<ASTNode> eNode = std::make_unique<ASTNode>("e", "var");
+    std::unique_ptr<ASTNode> fNode = std::make_unique<ASTNode>("f", "var");
+    std::unique_ptr<ASTNode> gNode = std::make_unique<ASTNode>("g", "var");
+    std::unique_ptr<ASTNode> hNode = std::make_unique<ASTNode>("h", "var");
+
+    std::vector<std::unique_ptr<ASTNode>> childrenW = {};
+    std::vector<std::unique_ptr<ASTNode>> childrenW2 = {};
+    std::vector<std::unique_ptr<ASTNode>> childrenW3 = {};
+    std::vector<std::unique_ptr<ASTNode>> childrenW4 = {};
+    std::vector<std::unique_ptr<ASTNode>> childrenW5 = {};
+    std::vector<std::unique_ptr<ASTNode>> childrenW6 = {};
+
+    childrenW.push_back(std::move(cNode));
+    childrenW.push_back(std::move(dNode));
+
+    auto lessThan = std::make_unique<ASTNode>("", "<", std::move(childrenW));
+    childrenW2.push_back(std::move(eNode));
+    childrenW2.push_back(std::move(fNode));
+
+    auto moreThan = std::make_unique<ASTNode>("", ">", std::move(childrenW2));
+    childrenW3.push_back(std::move(lessThan));
+    childrenW3.push_back(std::move(moreThan));
+    auto andandNode = std::make_unique<ASTNode>("", "&&", std::move(childrenW3));
+
+    childrenW4.push_back(std::move(gNode));
+    childrenW4.push_back(std::move(hNode));
+
+    auto assign = std::make_unique<ASTNode>("", "assign", std::move(childrenW4));
+    childrenW6.push_back(std::move(assign));
+    auto stmtList = std::make_unique<ASTNode>("", "stmtList", std::move(childrenW6));
+
+    childrenW5.push_back(std::move(andandNode));
+    childrenW5.push_back(std::move(stmtList));
+
+    auto whileNode = std::make_unique<ASTNode>("", "while", std::move(childrenW5));
+
     std::vector<std::unique_ptr<ASTNode>> children1 = {};
     std::vector<std::unique_ptr<ASTNode>> children2 = {};
 
