3259d205196b78fe5f97b1b9b2df2ccc8c711a4e
Ezekiel Toh <takufunkai@gmail.com>
2024-03-07 18:29:38 +0800

    Fix for mac/linux environments

--- Team09/Tests09/run_localhost.sh
+++ Team09/Tests09/run_localhost.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Team change to your python alias version "python3", "python", "py"
-PYTHON_EXECUTABLE="python"
+PYTHON_EXECUTABLE="python3"
 
-(cd  && "$PYTHON_EXECUTABLE" -m http.server 8001) & cd ..
-(cd  && "$PYTHON_EXECUTABLE" -m http.server 8002) & cd ..
-(cd  && "$PYTHON_EXECUTABLE" -m http.server 8003) & cd ..
+(cd milestone1 && "$PYTHON_EXECUTABLE" -m http.server 8001) &
+(cd milestone2 && "$PYTHON_EXECUTABLE" -m http.server 8002) & 
+(cd milestone3 && "$PYTHON_EXECUTABLE" -m http.server 8003) &
 
 wait
