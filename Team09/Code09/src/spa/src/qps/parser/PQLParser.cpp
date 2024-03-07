bcfd86112e706cb207c0cf2203531a12420d8571
juslam19 <e0552250@u.nus.edu>
2024-03-08 00:02:12 +0800

    fix: fix express-spec regex pt 2

--- Team09/Code09/src/spa/src/qps/parser/PQLParser.cpp
+++ Team09/Code09/src/spa/src/qps/parser/PQLParser.cpp
@@ -241,15 +241,15 @@ std::vector<ClauseArgument*> PQLParser::buildPatternParameters(const std::vector
     }
 
     // third argument is expression-spec
-    if (isExpressionSpec(ptExpressionSpec)) {
+    //if (isExpressionSpec(ptExpressionSpec)) {
         if (isWildcard(ptExpressionSpec)) {
             results.push_back(new Wildcard());
         } else {
             results.push_back(new ExpressionSpec(removeAllQuotations(removeAllWhitespaces(ptExpressionSpec))));
         }
-    } else {
-        throw Exception("Pattern Expression-spec is not expression-spec or wildcard: " + ptEntRef);
-    }
+    //} else {
+        //throw Exception("Pattern Expression-spec is not expression-spec or wildcard: " + ptEntRef);
+    //}
 
     return results;
 }
