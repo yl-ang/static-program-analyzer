65b09bee19d9627889e7e54de5e091715481a1fd
hanqinilnix <ohanqin@gmail.com>
2024-03-08 22:04:56 +0800

    Add proper validation for expression spec

--- Team09/Code09/src/spa/src/qps/GrammarUtils.cpp
+++ Team09/Code09/src/spa/src/qps/GrammarUtils.cpp
@@ -41,8 +41,118 @@ bool isRelRef(const std::string& str) {
 
 // TODO(Han Qin): Redo isExpressionSpec after Milestone 1.
 bool isExpressionSpec(const std::string& str) {
-    return std::regex_match(str, std::regex("^_\\s*\"(?:(?:0|[1-9][0-9]*)|[a-zA-Z][a-zA-Z0-9]*)\"\\s*_$")) ||
-           isWildcard(str);
+    if (isWildcard(str)) {
+        return true;
+    }
+
+    bool startsAndEndsWithUnderscores = std::regex_match(str, std::regex("^_.*_$"));
+    std::string removedUnderscoreString = str;
+    if (startsAndEndsWithUnderscores) {
+        removedUnderscoreString = trim(str.substr(1, str.size() - 2));
+    }
+    return isQuotedExpr(removedUnderscoreString);
+}
+
+bool isQuotedExpr(const std::string& str) {
+    bool startsAndEndsWithQuotes = std::regex_match(str, std::regex("^\".*\"$"));
+    if (startsAndEndsWithQuotes) {
+        std::string removedQuoteString = trim(str.substr(1, str.size() - 2));
+        return isExpr(removedQuoteString);
+    }
+
+    return false;
+}
+
+bool isExpr(const std::string& str) {
+    if (str.empty()) {
+        return false;
+    }
+
+    int index = -1;
+    int bracketCounter = 0;
+    for (int i = str.size() - 1; i >= 0; i--) {
+        if ((str[i] == '+' || str[i] == '-') && bracketCounter == 0) {
+            index = i;
+            break;
+        }
+        if (str[i] == ')') {
+            bracketCounter++;
+        }
+        if (str[i] == '(') {
+            bracketCounter--;
+        }
+        // Open bracket before close bracket (since we going from the back)
+        if (bracketCounter < 0) {
+            return false;
+        }
+    }
+
+    // Too many close brackets
+    if (bracketCounter > 0) {
+        return false;
+    }
+
+    // +/- not found, might be term
+    if (index == -1) {
+        return isTerm(str);
+    }
+
+    std::string expr = trim(str.substr(0, index));
+    std::string term = trim(str.substr(index + 1, str.size() - index - 1));
+    return isExpr(expr) && isTerm(term);
+}
+
+bool isTerm(const std::string& str) {
+    if (str.empty()) {
+        return false;
+    }
+
+    int index = -1;
+    int bracketCounter = 0;
+    for (int i = str.size() - 1; i >= 0; i--) {
+        if ((str[i] == '*' || str[i] == '/' || str[i] == '%') && bracketCounter == 0) {
+            index = i;
+            break;
+        }
+        if (str[i] == ')') {
+            bracketCounter++;
+        }
+        if (str[i] == '(') {
+            bracketCounter--;
+        }
+        // Open bracket before close bracket (since we going from the back)
+        if (bracketCounter < 0) {
+            return false;
+        }
+    }
+
+    // Too many close brackets
+    if (bracketCounter > 0) {
+        return false;
+    }
+
+    // +/- not found, might be factor
+    if (index == -1) {
+        return isFactor(str);
+    }
+
+    std::string term = trim(str.substr(0, index));
+    std::string factor = trim(str.substr(index + 1, str.size() - index - 1));
+    return isTerm(term) && isFactor(factor);
+}
+
+bool isFactor(const std::string& str) {
+    std::string trimmedString = trim(str);
+    if (isName(trimmedString) || isInteger(trimmedString)) {
+        return true;
+    }
+
+    bool startsAndEndsWithBrackets = std::regex_match(str, std::regex("^\\(.*\\)$"));
+    std::string expr = trim(trimmedString.substr(1, trimmedString.size() - 2));
+    if (startsAndEndsWithBrackets) {
+        return isExpr(expr);
+    }
+    return false;
 }
 
 bool isSelectStatement(const std::string& str) {
--- Team09/Code09/src/spa/src/qps/GrammarUtils.h
+++ Team09/Code09/src/spa/src/qps/GrammarUtils.h
@@ -16,7 +16,12 @@ bool isQuotedIdent(const std::string&);
 bool isStmtRef(const std::string&);
 bool isEntRef(const std::string&);
 bool isRelRef(const std::string&);
+
 bool isExpressionSpec(const std::string&);
+bool isQuotedExpr(const std::string& str);
+bool isExpr(const std::string&);
+bool isTerm(const std::string&);
+bool isFactor(const std::string&);
 
 bool isSelectStatement(const std::string&);
 bool isDeclarationStatement(const std::string&);
