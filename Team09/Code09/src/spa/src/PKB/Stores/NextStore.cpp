34bf5ab3060f083f328e1e8deaf7ba54649c6745
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-09 17:04:07 +0800

    Add skeleton implementation for next *

--- Team09/Code09/src/spa/src/PKB/Stores/NextStore.cpp
+++ Team09/Code09/src/spa/src/PKB/Stores/NextStore.cpp
@@ -24,12 +24,25 @@ std::unordered_set<StmtNum> NextStore::getNextee(StmtNum nexter) {
     return {};
 }
 
+std::unordered_set<StmtNum> NextStore::getNexterStar(StmtNum nextee) {
+    return computeNextStar(nextee, nexterMap);
+}
+
+std::unordered_set<StmtNum> NextStore::getNexteeStar(StmtNum nexter) {
+    return computeNextStar(nexter, nexteeMap);
+}
+
 bool NextStore::hasNextRelationship(StmtNum s1, StmtNum s2) {
     auto it = nexterMap.find(s1);
     if (it != nexterMap.end()) {
         return it->second.count(s2) > 0;
     }
     return false;
+}
+
+bool NextStore::hasNextStarRelationship(StmtNum s1, StmtNum s2) {
+    auto nexterSet = computeNextStar(s1, nexterMap);
+    return nexterSet.find(s2) != nexterSet.end();
 }
 
 bool NextStore::hasNextRelationship(ClauseArgument& arg1, ClauseArgument& arg2) {
@@ -48,4 +61,50 @@ bool NextStore::hasNextRelationship(ClauseArgument& arg1, ClauseArgument& arg2) 
     }
 
     return hasNextRelationship(std::stoi(arg1.getValue()), std::stoi(arg2.getValue()));
+}
+
+bool NextStore::hasNextStarRelationship(ClauseArgument& arg1, ClauseArgument& arg2) {
+    if (arg1.isWildcard() && arg2.isWildcard()) {
+        return !(nexterMap.empty());
+    }
+
+    // if arg 1 is wildcard, check if arg2 has nexteeStar
+    if (arg1.isWildcard()) {
+        return !getNexteeStar(std::stoi(arg2.getValue())).empty();
+    }
+
+    // if arg2 is wildcard, check if arg1 has nextersStar
+    if (arg2.isWildcard()) {
+        return !getNexterStar(std::stoi(arg1.getValue())).empty();
+    }
+
+    return hasNextStarRelationship(std::stoi(arg1.getValue()), std::stoi(arg2.getValue()));
+}
+
+// Helper function for depth-first search to find transitive closure
+void NextStore::dfsNextStar(StmtNum current, std::unordered_set<StmtNum>& result, std::unordered_set<StmtNum>& visited,
+                            const std::unordered_map<StmtNum, std::unordered_set<StmtNum>>& nextMap) {
+    if (visited.find(current) != visited.end()) {
+        return;
+    }
+
+    visited.insert(current);
+    result.insert(current);
+
+    auto nexterSet = nextMap.find(current);
+    if (nexterSet != nextMap.end()) {
+        for (StmtNum nextee : nexterSet->second) {
+            dfsNextStar(nextee, result, visited, nextMap);
+        }
+    }
+}
+
+std::unordered_set<StmtNum> NextStore::computeNextStar(
+    StmtNum start, const std::unordered_map<StmtNum, std::unordered_set<StmtNum>>& nextMap) {
+    std::unordered_set<StmtNum> result;
+    std::unordered_set<StmtNum> visited;
+
+    dfsNextStar(start, result, visited, nextMap);
+
+    return result;
 }
