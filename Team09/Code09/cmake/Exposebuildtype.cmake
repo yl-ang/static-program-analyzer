1ad5e3f299489fcad23cc4a2a62b4e04ea755b8d
wonyeji <e0564722@u.nus.edu>
2024-03-12 05:03:50 +0900

    Update Exposebuildtype.cmake

--- Team09/Code09/cmake/Exposebuildtype.cmake
+++ Team09/Code09/cmake/Exposebuildtype.cmake
@@ -1,6 +1,4 @@
 # Expose the build type as a corresponding preprocessor variable
-message(STATUS "ERROR: UNKNOWN BUILD TYPE")
-
 if(CMAKE_BUILD_TYPE MATCHES Debug)
     add_definitions(-DDEBUG_BUILD)
 elseif(CMAKE_BUILD_TYPE MATCHES Release)
