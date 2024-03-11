9ebed385e83bd2c77c46ac342abbd88d30808c93
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-12 00:13:49 +0800

    Add implementation for Modifies for Procedure

--- Team09/Code09/src/spa/src/PKB/Stores/ModifiesStore.cpp
+++ Team09/Code09/src/spa/src/PKB/Stores/ModifiesStore.cpp
@@ -1,12 +1,23 @@
 #include "ModifiesStore.h"
 
-void ModifiesStore::setModifiesStore(const std::unordered_set<std::pair<StmtNum, Variable>>& modifiesSet) {
+void ModifiesStore::setStatementModifiesStore(const std::unordered_set<std::pair<StmtNum, Variable>>& modifiesSet) {
     for (const auto& modify : modifiesSet) {
         StmtNum stmt = modify.first;
         Variable variable = modify.second;
 
         stmtToModifiedVariables[stmt].insert(variable);
         variableToModifyingStatements[variable].insert(stmt);
+    }
+}
+
+void ModifiesStore::setProcedureModifiesStore(
+    const std::unordered_set<std::pair<Procedure, Variable>>& modifiesProcedureSet) {
+    for (const auto& modify : modifiesProcedureSet) {
+        Procedure procedure = modify.first;
+        Variable variable = modify.second;
+
+        procedureToModifiedVariables[procedure].insert(variable);
+        variableToModifyingProcedures[variable].insert(procedure);
     }
 }
 
@@ -36,6 +47,32 @@ bool ModifiesStore::hasStatementVariableModifiesRelationship(ClauseArgument& arg
     return hasStatementVariableModifiesRelationship(std::stoi(arg1.getValue()), arg2.getValue());
 }
 
+bool ModifiesStore::hasProcedureVariableModifiesRelationship(Procedure procedure, const Variable& variable) const {
+    auto stmtIt = procedureToModifiedVariables.find(procedure);
+    if (stmtIt != procedureToModifiedVariables.end()) {
+        return stmtIt->second.count(variable) > 0;
+    }
+    return false;
+}
+
+bool ModifiesStore::hasProcedureVariableModifiesRelationship(ClauseArgument& arg1, ClauseArgument& arg2) const {
+    if (arg1.isWildcard() && arg2.isWildcard()) {
+        return !procedureToModifiedVariables.empty();
+    }
+
+    // if arg1 is wildcard, check if arg2 is modified by any statements
+    if (arg1.isWildcard()) {
+        return !getStatementsByVariable(arg2.getValue()).empty();
+    }
+
+    // if arg2 is wildcard, check if arg1 modifies any variables
+    if (arg2.isWildcard()) {
+        return !getVariablesByStatement(std::stoi(arg1.getValue())).empty();
+    }
+
+    return hasProcedureVariableModifiesRelationship(arg1.getValue(), arg2.getValue());
+}
+
 std::unordered_set<Variable> ModifiesStore::getVariablesByStatement(StmtNum stmt) const {
     auto stmtIt = stmtToModifiedVariables.find(stmt);
     if (stmtIt != stmtToModifiedVariables.end()) {
@@ -47,6 +84,22 @@ std::unordered_set<Variable> ModifiesStore::getVariablesByStatement(StmtNum stmt
 std::unordered_set<StmtNum> ModifiesStore::getStatementsByVariable(const Variable& variable) const {
     auto variableIt = variableToModifyingStatements.find(variable);
     if (variableIt != variableToModifyingStatements.end()) {
+        return variableIt->second;
+    }
+    return {};
+}
+
+std::unordered_set<Variable> ModifiesStore::getVariablesByProcedure(const Procedure& procedure) const {
+    auto procIt = procedureToModifiedVariables.find(procedure);
+    if (procIt != procedureToModifiedVariables.end()) {
+        return procIt->second;
+    }
+    return {};
+}
+
+std::unordered_set<Procedure> ModifiesStore::getProceduresByVariable(const Variable& variable) const {
+    auto variableIt = variableToModifyingProcedures.find(variable);
+    if (variableIt != variableToModifyingProcedures.end()) {
         return variableIt->second;
     }
     return {};
--- Team09/Code09/src/spa/src/PKB/Stores/ModifiesStore.h
+++ Team09/Code09/src/spa/src/PKB/Stores/ModifiesStore.h
@@ -11,13 +11,21 @@
 
 class ModifiesStore {
 public:
-    void setModifiesStore(const std::unordered_set<std::pair<StmtNum, Variable>>& modifiesSet);
+    // TODO(yl-ang): Rename setStatementModifiesStore -> setStatementModifiesStore to avoid confusion in SP side
+    void setStatementModifiesStore(const std::unordered_set<std::pair<StmtNum, Variable>>& modifiesSet);
+    void setProcedureModifiesStore(const std::unordered_set<std::pair<Procedure, Variable>>& modifiesProcedureSet);
     bool hasStatementVariableModifiesRelationship(StmtNum stmt, const Variable& variable) const;
     bool hasStatementVariableModifiesRelationship(ClauseArgument& arg1, ClauseArgument& arg2) const;
+    bool hasProcedureVariableModifiesRelationship(Procedure procedure, const Variable& variable) const;
+    bool hasProcedureVariableModifiesRelationship(ClauseArgument& arg1, ClauseArgument& arg2) const;
     std::unordered_set<Variable> getVariablesByStatement(StmtNum stmt) const;
     std::unordered_set<StmtNum> getStatementsByVariable(const Variable& variable) const;
+    std::unordered_set<Variable> getVariablesByProcedure(const Procedure& procedure) const;
+    std::unordered_set<Procedure> getProceduresByVariable(const Variable& variable) const;
 
 private:
     std::unordered_map<StmtNum, std::unordered_set<Variable>> stmtToModifiedVariables;
     std::unordered_map<Variable, std::unordered_set<StmtNum>> variableToModifyingStatements;
+    std::unordered_map<Procedure, std::unordered_set<Variable>> procedureToModifiedVariables;
+    std::unordered_map<Variable, std::unordered_set<Procedure>> variableToModifyingProcedures;
 };
