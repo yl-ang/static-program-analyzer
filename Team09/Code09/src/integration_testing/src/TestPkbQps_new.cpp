86f633db935f12da13f565ca8c67e98e05cd5129
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-13 15:30:25 +0800

    Update TestPkbQps_new.cpp

--- Team09/Code09/src/integration_testing/src/TestPkbQps_new.cpp
+++ Team09/Code09/src/integration_testing/src/TestPkbQps_new.cpp
@@ -85,7 +85,7 @@ PKBFacadeReader buildPKBNew(PKB pkb) {
     pfw.setFollowsStore(followsStoreEntries);
     pfw.setParentStore(parentStoreEntries);
     pfw.setModifiesStore(modifiesStoreEntries);
-    pfw.setUsesStore(usesStoreEntries);
+    pfw.setStatementUsesStore(usesStoreEntries);
     pfw.setPatternStore(patternStoreEntries);
 
     return PKBFacadeReader{pkb};
