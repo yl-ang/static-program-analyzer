dd1fd929de1be15206df54a93eada689523bd46a
Ezekiel Toh <takufunkai@gmail.com>
2024-03-15 22:01:39 +0800

    Add calls

---
+++ Team09/Code09/src/spa/src/qps/clauses/relationships/Calls.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "PKB/PKBClient/PKBFacadeReader.h"
+#include "Relationship.h"
+#include "qps/clauseArguments/ClauseArgument.h"
+#include "qps/clauseArguments/Integer.h"
+#include "qps/clauses/ClauseResult.h"
+
+class Calls : public Relationship {
+private:
+    ClauseArgument& caller;
+    ClauseArgument& callee;
+
+    /**
+     * Check if the result is a simple boolean result.
+     * It is a simple boolean result if both arguments are not synonyms.
+     * @return true if the result is a simple boolean result, false otherwise.
+     */
+    bool isSimpleResult() const;
+
+    ClauseResult evaluateCalleeSynonym(PKBFacadeReader& reader);
+    ClauseResult evaluateCallerSynonym(PKBFacadeReader&);
+    ClauseResult evaluateBothSynonyms(PKBFacadeReader&);
+
+public:
+    Calls(ClauseArgument&, ClauseArgument&);
+
+    ClauseResult evaluate(PKBFacadeReader&) override;
+};
