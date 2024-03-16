815c0d3b23e4e0fced6942b122bbdae61ce32437
wonyeji <e0564722@u.nus.edu>
2024-03-16 11:05:24 +0900

    Add Tokenizer tests

--- Team09/Code09/src/unit_testing/src/sp/TestTokenizer.cpp
+++ Team09/Code09/src/unit_testing/src/sp/TestTokenizer.cpp
@@ -11,33 +11,289 @@ TEST_CASE("Tokenizer Tests") {
         REQUIRE(tokenA == tokenB);
     }
 
-    SECTION("tokenize") {
-        std::vector<Token> expectedResultList = {Token(PROCEDURE, "procedure", -1),
-                                                 Token(NAME, "example", -1),
-                                                 Token(OPEN_CURLY_BRACE, "{", -1),
-                                                 Token(NAME, "x", 1),
-                                                 Token(EQUAL, "=", 1),
-                                                 Token(INTEGER, "1", 1),
-                                                 Token(SEMICOLON, ";", 1),
-                                                 Token(WHILE, "while", 2),
-                                                 Token(OPEN_BRACKET, "(", 2),
-                                                 Token(NAME, "x", 2),
-                                                 Token(GREATER_THAN, ">", 2),
-                                                 Token(INTEGER, "0", 2),
-                                                 Token(CLOSE_BRACKET, ")", 2),
-                                                 Token(OPEN_CURLY_BRACE, "{", 2),
-                                                 Token(NAME, "x", 3),
-                                                 Token(EQUAL, "=", 3),
-                                                 Token(NAME, "x", 3),
-                                                 Token(SUB, "-", 3),
-                                                 Token(INTEGER, "1", 3),
-                                                 Token(SEMICOLON, ";", 3),
-                                                 Token(CLOSE_CURLY_BRACE, "}", -1),
+    SECTION("Tokenize PROCEDURE correctly") {
+        std::vector<Token> expectedResultList = {Token(PROCEDURE, "procedure", -1)};
+        std::vector<BasicToken*> input = {new BasicToken("procedure", KEYWORD)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize OPEN_CURLY_BRACE correctly") {
+        std::vector<Token> expectedResultList = {Token(OPEN_CURLY_BRACE, "{", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("{", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize CLOSE_CURLY_BRACE correctly") {
+        std::vector<Token> expectedResultList = {Token(CLOSE_CURLY_BRACE, "}", -1)};
+        std::vector<BasicToken*> input = {new BasicToken("}", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize SEMICOLON correctly") {
+        std::vector<Token> expectedResultList = {Token(SEMICOLON, ";", 0)};
+        std::vector<BasicToken*> input = {new BasicToken(";", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize NAME correctly") {
+        std::vector<Token> expectedResultList = {Token(NAME, "happy", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("happy", _NAME)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize INTEGER correctly") {
+        std::vector<Token> expectedResultList = {Token(INTEGER, "123843", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("123843", _INTEGER)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize EQUAL correctly") {
+        std::vector<Token> expectedResultList = {Token(EQUAL, "=", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("=", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize READ correctly") {
+        std::vector<Token> expectedResultList = {Token(READ, "read", 1)};
+        std::vector<BasicToken*> input = {new BasicToken("read", KEYWORD)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize PRINT correctly") {
+        std::vector<Token> expectedResultList = {Token(PRINT, "print", 1)};
+        std::vector<BasicToken*> input = {new BasicToken("print", KEYWORD)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize CALL correctly") {
+        std::vector<Token> expectedResultList = {Token(CALL, "call", 1)};
+        std::vector<BasicToken*> input = {new BasicToken("call", KEYWORD)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize IF correctly") {
+        std::vector<Token> expectedResultList = {Token(IF, "if", 1)};
+        std::vector<BasicToken*> input = {new BasicToken("if", KEYWORD)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize OPEN_BRACKET correctly") {
+        std::vector<Token> expectedResultList = {Token(OPEN_BRACKET, "(", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("(", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+    SECTION("Tokenize CLOSE_BRACKET correctly") {
+        std::vector<Token> expectedResultList = {Token(CLOSE_BRACKET, ")", 0)};
+        std::vector<BasicToken*> input = {new BasicToken(")", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+    SECTION("Tokenize THEN correctly") {
+        std::vector<Token> expectedResultList = {Token(THEN, "then", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("then", KEYWORD)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+    SECTION("Tokenize ELSE correctly") {
+        std::vector<Token> expectedResultList = {Token(ELSE, "else", -1)};
+        std::vector<BasicToken*> input = {new BasicToken("else", KEYWORD)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize NOT correctly") {
+        std::vector<Token> expectedResultList = {Token(NOT, "!", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("!", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize ANDAND correctly") {
+        std::vector<Token> expectedResultList = {Token(ANDAND, "&&", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("&&", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize OR correctly") {
+        std::vector<Token> expectedResultList = {Token(OR, "||", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("||", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize LESS_THAN correctly") {
+        std::vector<Token> expectedResultList = {Token(LESS_THAN, "<", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("<", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize LESS_THAN_OR_EQUAL_TO correctly") {
+        std::vector<Token> expectedResultList = {Token(LESS_THAN_OR_EQUAL_TO, "<=", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("<=", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize GREATER_THAN correctly") {
+        std::vector<Token> expectedResultList = {Token(GREATER_THAN, ">", 0)};
+        std::vector<BasicToken*> input = {new BasicToken(">", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize GREATER_THAN_OR_EQUAL_TO correctly") {
+        std::vector<Token> expectedResultList = {Token(GREATER_THAN_OR_EQUAL_TO, ">=", 0)};
+        std::vector<BasicToken*> input = {new BasicToken(">=", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize EQUAL_CHECK correctly") {
+        std::vector<Token> expectedResultList = {Token(EQUAL_CHECK, "==", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("==", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize NOT_EQUAL_CHECK correctly") {
+        std::vector<Token> expectedResultList = {Token(NOT_EQUAL_CHECK, "!=", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("!=", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize ADD correctly") {
+        std::vector<Token> expectedResultList = {Token(ADD, "+", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("+", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize SUB correctly") {
+        std::vector<Token> expectedResultList = {Token(SUB, "-", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("-", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize MUL correctly") {
+        std::vector<Token> expectedResultList = {Token(MUL, "*", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("*", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize DIV correctly") {
+        std::vector<Token> expectedResultList = {Token(DIV, "/", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("/", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Tokenize MOD correctly") {
+        std::vector<Token> expectedResultList = {Token(MOD, "%", 0)};
+        std::vector<BasicToken*> input = {new BasicToken("%", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Procedure definitions should not have statement numbers") {
+        std::vector<Token> expectedResultList = {Token(PROCEDURE, "procedure", -1), Token(NAME, "example", -1),
+                                                 Token(OPEN_CURLY_BRACE, "{", -1),  Token(READ, "read", 1),
+                                                 Token(NAME, "example", 1),         Token(SEMICOLON, ";", 1),
                                                  Token(CLOSE_CURLY_BRACE, "}", -1)};
-        std::vector<std::string> inputString = {"procedure", "example", "{", "x", "=", "1", ";", "while",
-                                                "(",         "x",       ">", "0", ")", "{", "x", "=",
-                                                "x",         "-",       "1", ";", "}", "}"};
-        std::vector<Token> resultList = tokenizer.tokenize(inputString);
+
+        std::vector<BasicToken*> input = {new BasicToken("procedure", KEYWORD), new BasicToken("example", _NAME),
+                                          new BasicToken("{", SYMBOL),          new BasicToken("read", KEYWORD),
+                                          new BasicToken("example", _NAME),     new BasicToken(";", SYMBOL),
+                                          new BasicToken("}", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Statement number correctly handled for while statements") {
+        std::vector<Token> expectedResultList = {Token(WHILE, "while", 1),     Token(OPEN_BRACKET, "(", 1),
+                                                 Token(CLOSE_BRACKET, ")", 1), Token(OPEN_CURLY_BRACE, "{", 1),
+                                                 Token(READ, "read", 2),       Token(NAME, "example", 2),
+                                                 Token(SEMICOLON, ";", 2),     Token(CLOSE_CURLY_BRACE, "}", -1)};
+
+        std::vector<BasicToken*> input = {new BasicToken("while", KEYWORD), new BasicToken("(", SYMBOL),
+                                          new BasicToken(")", SYMBOL),      new BasicToken("{", SYMBOL),
+                                          new BasicToken("read", KEYWORD),  new BasicToken("example", _NAME),
+                                          new BasicToken(";", SYMBOL),      new BasicToken("}", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Statement number correctly handled for if statements") {
+        std::vector<Token> expectedResultList = {
+            Token(IF, "if", 1),        Token(OPEN_BRACKET, "(", 1),      Token(CLOSE_BRACKET, ")", 1),
+            Token(THEN, "then", 1),    Token(OPEN_CURLY_BRACE, "{", 1),  Token(READ, "read", 2),
+            Token(NAME, "example", 2), Token(SEMICOLON, ";", 2),         Token(CLOSE_CURLY_BRACE, "}", -1),
+            Token(ELSE, "else", -1),   Token(OPEN_CURLY_BRACE, "{", -1), Token(READ, "read", 3),
+            Token(NAME, "example", 3), Token(SEMICOLON, ";", 3),         Token(CLOSE_CURLY_BRACE, "}", -1)};
+
+        std::vector<BasicToken*> input = {
+            new BasicToken("if", KEYWORD),    new BasicToken("(", SYMBOL), new BasicToken(")", SYMBOL),
+            new BasicToken("then", KEYWORD),  new BasicToken("{", SYMBOL), new BasicToken("read", KEYWORD),
+            new BasicToken("example", _NAME), new BasicToken(";", SYMBOL), new BasicToken("}", SYMBOL),
+            new BasicToken("else", KEYWORD),  new BasicToken("{", SYMBOL), new BasicToken("read", KEYWORD),
+            new BasicToken("example", _NAME), new BasicToken(";", SYMBOL), new BasicToken("}", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Statement number correctly handled for read statements") {
+        std::vector<Token> expectedResultList = {Token(READ, "read", 1), Token(NAME, "happy", 1),
+                                                 Token(SEMICOLON, ";", 1)};
+
+        std::vector<BasicToken*> input = {new BasicToken("read", KEYWORD), new BasicToken("happy", _NAME),
+                                          new BasicToken(";", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Statement number correctly handled for assign statements") {
+        std::vector<Token> expectedResultList = {Token(NAME, "x", 1), Token(EQUAL, "=", 1), Token(INTEGER, "4", 1),
+                                                 Token(SEMICOLON, ";", 1)};
+
+        std::vector<BasicToken*> input = {new BasicToken("x", _NAME), new BasicToken("=", SYMBOL),
+                                          new BasicToken("4", _INTEGER), new BasicToken(";", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Statement number correctly handled for call statements") {
+        std::vector<Token> expectedResultList = {Token(CALL, "call", 1), Token(NAME, "happy", 1),
+                                                 Token(SEMICOLON, ";", 1)};
+
+        std::vector<BasicToken*> input = {new BasicToken("call", KEYWORD), new BasicToken("happy", _NAME),
+                                          new BasicToken(";", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
+        REQUIRE(expectedResultList == resultList);
+    }
+
+    SECTION("Statement number correctly handled for print statements") {
+        std::vector<Token> expectedResultList = {Token(PRINT, "print", 1), Token(NAME, "happy", 1),
+                                                 Token(SEMICOLON, ";", 1)};
+
+        std::vector<BasicToken*> input = {new BasicToken("print", KEYWORD), new BasicToken("happy", _NAME),
+                                          new BasicToken(";", SYMBOL)};
+        std::vector<Token> resultList = tokenizer.assignTokens(input);
         REQUIRE(expectedResultList == resultList);
     }
 }
