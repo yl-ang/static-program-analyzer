b0ac6c4d05a6bfdfbab6655317f44ec97aedd22f
wonyeji <e0564722@u.nus.edu>
2024-03-16 12:10:51 +0900

    add calls extractor skeleton

--- Team09/Code09/src/spa/src/sp/de/AbstractionExtractor.h
+++ Team09/Code09/src/spa/src/sp/de/AbstractionExtractor.h
@@ -1,7 +1,9 @@
 #pragma once
+#include <memory>
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 
 #include "../../PKB/PKBClient/PKBFacadeWriter.h"
 #include "../../PKB/utils/DataTypes.h"
@@ -19,4 +21,5 @@ public:
     std::unordered_set<std::pair<StmtNum, Variable>> uses;
     std::unordered_set<std::pair<StmtNum, std::pair<std::string, std::string>>> pattern;
     std::unordered_map<StmtNum, std::pair<std::string, std::shared_ptr<Matchable>>> matchablePattern;
+    std::unordered_set<std::pair<Procedure, Procedure>> calls;
 };
