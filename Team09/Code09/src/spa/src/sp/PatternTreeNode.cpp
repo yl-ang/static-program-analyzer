1dd9355758b27bdeff0a697b688797c3a3bd09d0
benluiwj <wenjie.lui@gmail.com>
2024-03-17 22:10:40 +0800

    add serialising and deserialising

--- Team09/Code09/src/spa/src/sp/PatternTreeNode.cpp
+++ Team09/Code09/src/spa/src/sp/PatternTreeNode.cpp
@@ -1,11 +1,15 @@
 #include "PatternTreeNode.h"
 
+#include <memory>
+#include <sstream>
+
 #include "queue"
 #include "sp/SourceLoader.h"
 #include "sp/ast/Ast.h"
 #include "sp/ast/grammar_nodes/ExpressionNode.h"
 #include "sp/tokenizer/SpTokenizer.h"
-#include "sstream"
+
+const char SERIALISING_DELIMITER[] = "#";
 
 PatternTreeNode PatternTreeNode::buildTreeFromString(std::string value) {
     SourceLoader sourceLoader;
@@ -32,5 +36,35 @@ PatternTreeNode PatternTreeNode::buildTreeFromAST(std::shared_ptr<ExpressionNode
     if (node->right != nullptr) {
         root.right = std::make_shared<PatternTreeNode>(buildTreeFromAST(node->right));
     }
+    return root;
+}
+
+std::string PatternTreeNode::serialiseToString(std::shared_ptr<PatternTreeNode> node) {
+    std::ostringstream out;
+    serializeHelper(node, out);
+    return out.str();
+}
+void PatternTreeNode::serializeHelper(std::shared_ptr<PatternTreeNode> node, std::ostringstream& out) {
+    if (node != nullptr) {
+        out << node->value;
+        serializeHelper(node->left, out);
+        serializeHelper(node->right, out);
+    } else {
+        out << SERIALISING_DELIMITER;
+    }
+}
+
+std::shared_ptr<PatternTreeNode> PatternTreeNode::deserializeToNode(std::string value) {
+    std::istringstream in(value);
+    return deserializeHelper(in);
+}
+std::shared_ptr<PatternTreeNode> PatternTreeNode::deserializeHelper(std::istringstream& in) {
+    std::string val;
+    in >> val;
+    if (val == SERIALISING_DELIMITER)
+        return nullptr;
+    std::shared_ptr<PatternTreeNode> root = std::make_shared<PatternTreeNode>(PatternTreeNode(val));
+    root->left = deserializeHelper(in);
+    root->right = deserializeHelper(in);
     return root;
 }
--- Team09/Code09/src/spa/src/sp/PatternTreeNode.h
+++ Team09/Code09/src/spa/src/sp/PatternTreeNode.h
@@ -2,15 +2,20 @@
 
 #include "sp/ast/grammar_nodes/ExpressionNode.h"
 #include "string"
-
+// Tree serialising and deserialising adapted from the following solutions:
+// https://leetcode.com/problems/serialize-and-deserialize-binary-tree/solutions/74259/recursive-preorder-python-and-c-o-n/
 class PatternTreeNode {
     std::string value;
     std::shared_ptr<PatternTreeNode> left;
     std::shared_ptr<PatternTreeNode> right;
     PatternTreeNode(std::string value) : value(value), left(NULL), right(NULL) {}
     PatternTreeNode();
-    static PatternTreeNode buildTreeFromString(std::string value);
+    PatternTreeNode buildTreeFromString(std::string value);
+    std::string serialiseToString(std::shared_ptr<PatternTreeNode> node);
+    std::shared_ptr<PatternTreeNode> deserializeToNode(std::string);
 
 private:
-    static PatternTreeNode buildTreeFromAST(std::shared_ptr<ExpressionNode> expressoinNode);
+    PatternTreeNode buildTreeFromAST(std::shared_ptr<ExpressionNode> expressoinNode);
+    void serializeHelper(std::shared_ptr<PatternTreeNode> node, std::ostringstream& out);
+    std::shared_ptr<PatternTreeNode> deserializeHelper(std::istringstream& in);
 };
