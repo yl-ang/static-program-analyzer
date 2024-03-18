3b2e38532ed8fb4777fb0a97979c92c5e8925770
Ezekiel Toh <takufunkai@gmail.com>
2024-03-18 17:36:19 +0800

    Add clause evaluator utils

---
+++ Team09/Code09/src/spa/src/qps/clauses/ClauseEvaluatorUtils.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <unordered_set>
+
+#include "pkb/PKBClient/PKBFacadeReader.h"
+#include "qps/clauseArguments/Synonym.h"
+#include "qps/clauses/relationships/Relationship.h"
+
+class ClauseEvaluatorUtils {
+public:
+    static std::unordered_set<StmtNum> filterStatementsByType(PKBFacadeReader&, DesignEntityType,
+                                                              std::unordered_set<StmtNum>);
+    static bool isIdentLiteral(const std::string& str);
+};
