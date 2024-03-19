c64afac204b0a78fa12bfb7714fff1c1882ccf8e
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-19 15:24:58 +0800

    Update TestAssignPatternStore.cpp

--- Team09/Code09/src/unit_testing/src/PKB/Stores/TestAssignPatternStore.cpp
+++ Team09/Code09/src/unit_testing/src/PKB/Stores/TestAssignPatternStore.cpp
@@ -10,22 +10,37 @@ bool alwaysTrueFunction(std::string s1, std::string s2) {
 }
 
 bool alwaysFalseFunction(std::string s1, std::string s2) {
-    return s1 == s2;
+    return s1 != s2;
 }
+
 TEST_CASE("store - All Tests") {
     AssignPatternStore store;
 
     SECTION("Test always returns true") {
         std::unordered_set<std::pair<StmtNum, std::pair<std::string, std::string>>> patterns = {
-            {1, {"x", "y"}}, {2, {"a", "b"}}, {3, {"p", "q"}}};
-        std::function<bool(std::string, std::string)> funct_ptr = alwaysTrueFunction;
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysFalseFunction;
+            {1, {"x", "y + 1"}}, {2, {"a", "x * y * z"}}, {3, {"p", "q + 1 + x + y"}}};
+        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysTrueFunction;
+        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseFunction;
 
-        store.initialiseStore(funct_ptr, funct_ptr1, patterns);
+        store.initialiseStore(funct_ptr1, funct_ptr2, patterns);
 
-        std::string wildcardArg1 = "_";
-        std::string synonymsArg2 = "y";
+        std::string arg1 = "p";
+        std::string arg2 = "q + 1 + x + y";
 
-        REQUIRE(store.hasExactPattern(1, wildcardArg1, synonymsArg2));
+        REQUIRE(store.hasExactPattern(3, arg1, arg2));
+    }
+
+    SECTION("Test always returns true") {
+        std::unordered_set<std::pair<StmtNum, std::pair<std::string, std::string>>> patterns = {
+            {1, {"x", "y + 6"}}, {2, {"a", "b"}}, {3, {"p", "q"}}};
+        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysTrueFunction;
+        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseFunction;
+
+        store.initialiseStore(funct_ptr1, funct_ptr2, patterns);
+
+        std::string arg1 = "x";
+        std::string arg2 = "y";
+
+        REQUIRE_FALSE(store.hasPartialPattern(1, arg1, arg2));
     }
 }
