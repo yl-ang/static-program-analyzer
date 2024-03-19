8a89163102469f80ef2076c6e5102fdcd0414b07
wonyeji <e0564722@u.nus.edu>
2024-03-19 11:31:15 +0800

    Initialise tests

--- Team09/Code09/src/integration_testing/src/TestSpPkb.cpp
+++ Team09/Code09/src/integration_testing/src/TestSpPkb.cpp
@@ -5,69 +5,68 @@
 #include "Spa.h"
 #include "catch.hpp"
 
-TEST_CASE("Test sp and pkb - basic source.txt") {
+TEST_CASE("SP-PKB: Design Entities stored correctly") {
     SPA spa;
-    // Team, this path is for vs studio, as the integration binary is running from the out folder... (Mac user take
-    // note)
+    // This path is for VS, as the integration binary is running from the out folder... (Mac user take note)
     spa.parse("../../../../../src/integration_testing/test_files/basic_source.txt");
     PKB& pkb = spa.getInternalPKB();
     PKBFacadeReader pkbFacadeReader(pkb);
 
-    SECTION("SP-PKB Entity:Procedure") {
+    SECTION("Variables stored correctly") {
+        std::unordered_set<Variable> variableSet = pkbFacadeReader.getVariables();
+        std::unordered_set<Variable> expectedVariableSet = {"elephant", "iteration"};
+        REQUIRE(variableSet == expectedVariableSet);
+    }
+
+    SECTION("Constants stored correctly") {
+        std::unordered_set<Constant> constantsSet = pkbFacadeReader.getConstants();
+        std::unordered_set<Constant> expectedConstantsSet = {"elephant", "iteration"};
+        REQUIRE(constantsSet == expectedConstantsSet);
+    }
+
+    SECTION("Statements stored correctly") {
+        std::unordered_set<Stmt> stmtSet = pkbFacadeReader.getStmts();
+        std::unordered_set<Stmt> expectedStmtSet = {{StatementType::READ, 1}, {StatementType::ASSIGN, 2}};
+        REQUIRE(stmtSet == expectedStmtSet);
+    }
+
+    SECTION("Procedures stored correctly") {
         std::unordered_set<Procedure> procSet = pkbFacadeReader.getProcedures();
         std::unordered_set<Procedure> expectedProcSet = {"Animals"};
         REQUIRE(procSet == expectedProcSet);
     }
+}
 
-    SECTION("SP PKB get variables") {
-        std::unordered_set<Variable> variableSet = pkbFacadeReader.getVariables();
-        std::unordered_set<Variable> expectedVariableSet = {
-            "elephant", "iteration",   "additionalVar1", "totalAnimals", "additionalVar2",
-            "tiger",    "monkeyCount", "lionCount",      "newVar",       "x",
-            "k"};
-        REQUIRE(variableSet == expectedVariableSet);
-    }
+TEST_CASE("SP-PKB: Design Abstractions stored correctly") {
+    SPA spa;
+    // This path is for VS, as the integration binary is running from the out folder... (Mac user take note)
+    spa.parse("../../../../../src/integration_testing/test_files/basic_source.txt");
+    PKB& pkb = spa.getInternalPKB();
+    PKBFacadeReader pkbFacadeReader(pkb);
 
-    SECTION("SP PKB get constants") {
-        std::unordered_set<Constant> constantSet = pkbFacadeReader.getConstants();
-        std::unordered_set<Constant> expectedConstantSet = {
-            "1",    "100",      "0",    "332",  "42",    "333", "5",    "1234",   "2",       "9999", "111", "222",
-            "8888", "22228888", "2024", "2020", "10101", "17",  "1111", "641111", "9092229", "50",   "3"};
-        REQUIRE(constantSet == expectedConstantSet);
-    }
-
-    // TODO(yl-ang): SP PKB Statements tests
-
-    SECTION("SP-PKB Follows Relation") {
+    SECTION("Follows relationship stored correctly") {
         REQUIRE(pkbFacadeReader.hasFollowRelationship(1, 2));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(2, 3));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(3, 4));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(4, 5));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(5, 6));
         REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(6, 7));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(7, 8));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(8, 9));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(9, 10));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(10, 11));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(11, 12));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(12, 13));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(13, 14));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(14, 15));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(15, 16));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(16, 17));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(17, 18));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(18, 19));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(19, 20));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(20, 21));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(21, 22));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(22, 23));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(6, 22));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(6, 23));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(23, 24));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(23, 26));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(23, 27));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(24, 25));
-        REQUIRE(pkbFacadeReader.hasFollowRelationship(25, 26));
-        REQUIRE_FALSE(pkbFacadeReader.hasFollowRelationship(26, 27));
     }
+
+    SECTION("Parent relationship stored correctly") {
+        REQUIRE(pkbFacadeReader.hasParentRelationship(1, 2));
+        REQUIRE_FALSE(pkbFacadeReader.hasParentRelationship(6, 7));
+    }
+
+    SECTION("Uses relationship stored correctly - statements") {}
+
+    SECTION("Uses relationship stored correctly - procedures") {}
+
+    SECTION("Modifies relationship stored correctly - statements") {}
+
+    SECTION("Modifies relationship stored correctly - procedures") {}
+
+    SECTION("Calls relationship stored correctly") {}
+
+    SECTION("Follows relationship stored correctly") {}
+
+    SECTION("Next relationship stored correctly") {}
+
+    SECTION("Pattern stored correctly") {}
 }
