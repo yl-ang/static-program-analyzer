1fb2a7ce82c5f649ff60aaaadef70655f57283c5
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-20 10:01:00 +0800

    Implement call stmt map for call store
    As requested by QPS

--- Team09/Code09/src/spa/src/PKB/Stores/CallStore.h
+++ Team09/Code09/src/spa/src/PKB/Stores/CallStore.h
@@ -13,10 +13,14 @@
 class CallStore {
 public:
     void setCallStore(const std::unordered_set<std::pair<Procedure, Procedure>>& callPairs);
+    void setCallStmtStore(const std::unordered_set<std::pair<Procedure, StmtNum>>& callStmtPairs);
+
     std::unordered_set<Procedure> getCaller(Procedure callee);
     std::unordered_set<Procedure> getCallee(Procedure caller);
     std::unordered_set<Procedure> getCallerStar(Procedure callee);
     std::unordered_set<Procedure> getCalleeStar(Procedure caller);
+    std::unordered_set<StmtNum> getStmtNumFromCall(Procedure call);
+    Procedure getCallFromStmtNum(StmtNum stmtNum);
     bool hasCallRelationship(Procedure caller, Procedure callee);
     bool hasCallRelationship(ClauseArgument& arg1, ClauseArgument& arg2);
     bool hasCallStarRelationship(Procedure caller, Procedure callee);
@@ -27,6 +31,9 @@ private:
     std::unordered_map<Procedure, std::unordered_set<Procedure>> calleeToCallerMap;
     std::unordered_map<Procedure, std::unordered_set<Procedure>> callerToCalleeStarMap;
     std::unordered_map<Procedure, std::unordered_set<Procedure>> calleeToCallerStarMap;
+
+    std::unordered_map<Procedure, std::unordered_set<StmtNum>> callToStmtNumberMap;
+    std::unordered_map<StmtNum, Procedure> StmtNumberToCallMap;
 
     void computeTransitiveClosure();
 };
