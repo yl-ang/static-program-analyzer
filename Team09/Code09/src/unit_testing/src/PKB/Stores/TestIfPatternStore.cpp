997b2482f167e3c4e52f840f1f5050956ba591aa
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-21 21:07:36 +0800

    Update UTs for while and if pattern stores

--- Team09/Code09/src/unit_testing/src/PKB/Stores/TestIfPatternStore.cpp
+++ Team09/Code09/src/unit_testing/src/PKB/Stores/TestIfPatternStore.cpp
@@ -2,60 +2,23 @@
 
 #include "PKB/Stores/IfPatternStore.h"
 #include "catch.hpp"
-#include "qps/clauseArguments/Wildcard.h"
-
-bool alwaysTrueIfFunction(std::string s1, std::string s2) {
-    return true;
-}
-
-bool alwaysFalseIfFunction(std::string s1, std::string s2) {
-    return false;
-}
 
 TEST_CASE("IfPatternStore - All Tests") {
     IfPatternStore ifPatternStore;
+    std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
+    ifPatternStore.setIfPatternStore(patterns);
 
-    SECTION("Test always return true for hasExactPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysTrueIfFunction;
-        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseIfFunction;
-
-        ifPatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
-
-        std::string arg = "z";
-        REQUIRE(ifPatternStore.hasExactPattern(3, arg));
+    SECTION("Test return true for hasIfPattern - (StmtNum, Var)") {
+        REQUIRE(ifPatternStore.hasIfPattern(1, "x"));
+        REQUIRE(ifPatternStore.hasIfPattern(1, "q"));
+        REQUIRE(ifPatternStore.hasIfPattern(2, "y"));
+        REQUIRE(ifPatternStore.hasIfPattern(3, "z"));
     }
 
-    SECTION("Test always return false for hasExactPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysFalseIfFunction;
-        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseIfFunction;
-
-        ifPatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
-
-        std::string arg = "z";
-        REQUIRE_FALSE(ifPatternStore.hasExactPattern(3, arg));
-    }
-
-    SECTION("Test always return true for hasPartialPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysTrueIfFunction;
-        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysTrueIfFunction;
-
-        ifPatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
-
-        std::string arg = "z";
-        REQUIRE(ifPatternStore.hasPartialPattern(1, arg));
-    }
-
-    SECTION("Test always return false for hasPartialPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysFalseIfFunction;
-        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseIfFunction;
-
-        ifPatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
-
-        std::string arg = "z";
-        REQUIRE_FALSE(ifPatternStore.hasPartialPattern(1, arg));
+    SECTION("Test always return false for hasIfPattern - (StmtNum, Var)") {
+        REQUIRE_FALSE(ifPatternStore.hasIfPattern(1, "a"));
+        REQUIRE_FALSE(ifPatternStore.hasIfPattern(2, "b"));
+        REQUIRE_FALSE(ifPatternStore.hasIfPattern(3, "c"));
+        REQUIRE_FALSE(ifPatternStore.hasIfPattern(4, "d"));
     }
 }
--- Team09/Code09/src/unit_testing/src/PKB/Stores/TestWhilePatternStore.cpp
+++ Team09/Code09/src/unit_testing/src/PKB/Stores/TestWhilePatternStore.cpp
@@ -2,60 +2,23 @@
 
 #include "PKB/Stores/WhilePatternStore.h"
 #include "catch.hpp"
-#include "qps/clauseArguments/Wildcard.h"
-
-bool alwaysTrueWhileFunction(std::string s1, std::string s2) {
-    return true;
-}
-
-bool alwaysFalseWhileFunction(std::string s1, std::string s2) {
-    return false;
-}
 
 TEST_CASE("WhilePatternStore - All Tests") {
     WhilePatternStore whilePatternStore;
+    std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
+    whilePatternStore.setWhilePatternStore(patterns);
 
-    SECTION("Test always return true for hasExactPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysTrueWhileFunction;
-        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseWhileFunction;
-
-        whilePatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
-
-        std::string arg = "z";
-        REQUIRE(whilePatternStore.hasExactPattern(3, arg));
+    SECTION("Test return true for hasWhilePattern - (StmtNum, Var)") {
+        REQUIRE(whilePatternStore.hasWhilePattern(1, "x"));
+        REQUIRE(whilePatternStore.hasWhilePattern(1, "q"));
+        REQUIRE(whilePatternStore.hasWhilePattern(2, "y"));
+        REQUIRE(whilePatternStore.hasWhilePattern(3, "z"));
     }
 
-    SECTION("Test always return false for hasExactPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysFalseWhileFunction;
-        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseWhileFunction;
-
-        whilePatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
-
-        std::string arg = "z";
-        REQUIRE_FALSE(whilePatternStore.hasExactPattern(3, arg));
-    }
-
-    SECTION("Test always return true for hasPartialPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysTrueWhileFunction;
-        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysTrueWhileFunction;
-
-        whilePatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
-
-        std::string arg = "z";
-        REQUIRE(whilePatternStore.hasPartialPattern(1, arg));
-    }
-
-    SECTION("Test always return false for hasPartialPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
-        std::function<bool(std::string, std::string)> funct_ptr1 = alwaysFalseWhileFunction;
-        std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseWhileFunction;
-
-        whilePatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
-
-        std::string arg = "z";
-        REQUIRE_FALSE(whilePatternStore.hasPartialPattern(1, arg));
+    SECTION("Test always return false for hasWhilePattern - (StmtNum, Var)") {
+        REQUIRE_FALSE(whilePatternStore.hasWhilePattern(1, "a"));
+        REQUIRE_FALSE(whilePatternStore.hasWhilePattern(2, "b"));
+        REQUIRE_FALSE(whilePatternStore.hasWhilePattern(3, "c"));
+        REQUIRE_FALSE(whilePatternStore.hasWhilePattern(4, "d"));
     }
 }
