a9a5ec78c9ea3402154c99910102b75a5f948af2
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-21 16:29:35 +0800

    Add ifPatternStore

---
+++ Team09/Code09/src/spa/src/PKB/Stores/IfPatternStore.cpp
@@ -0,0 +1,42 @@
+#include "IfPatternStore.h"
+
+#include <algorithm>
+#include <functional>
+
+#include "PKB/Utils/DataTypes.h"
+
+void IfPatternStore::initialiseIfPatternStore(std::function<bool(std::string, std::string)> exactMatchFP,
+                                              std::function<bool(std::string, std::string)> partialMatchFP,
+                                              std::unordered_set<std::pair<StmtNum, std::string>> ifPatternPairs) {
+    this->exactMatchFP = exactMatchFP;
+    this->partialMatchFP = partialMatchFP;
+    setIfPatterns(ifPatternPairs);
+}
+
+void IfPatternStore::setIfPatterns(const std::unordered_set<std::pair<StmtNum, std::string>>& ifPatternPairs) {
+    for (const auto& pattern : ifPatternPairs) {
+        const auto& condExpr = pattern.second;
+        ifPatternsMap[pattern.first] = condExpr;
+    }
+}
+
+bool IfPatternStore::hasExactPattern(StmtNum stmtNum, std::string expr) {
+    return applyIfPatternFunction(exactMatchFP, stmtNum, expr);
+}
+
+bool IfPatternStore::hasPartialPattern(StmtNum stmtNum, std::string expr) {
+    return applyIfPatternFunction(partialMatchFP, stmtNum, expr);
+}
+
+bool IfPatternStore::applyIfPatternFunction(std::function<bool(std::string, std::string)> function, StmtNum stmtNum,
+                                            std::string expr) {
+    auto it = ifPatternsMap.find(stmtNum);
+
+    // Check if stmtNum is found
+    if (it != ifPatternsMap.end()) {
+        // Retrieve the expr associated with stmtNum
+        const auto& IfPatternExpr = it->second;
+        return function(IfPatternExpr, expr);
+    }
+    return false;
+}
---
+++ Team09/Code09/src/spa/src/PKB/Stores/IfPatternStore.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <qps/clauseArguments/ClauseArgument.h>
+
+#include <functional>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+#include "PKB/Utils/DataTypes.h"
+
+class IfPatternStore {
+public:
+    void initialiseStore(std::function<bool(std::string, std::string)> exactMatchFP,
+                         std::function<bool(std::string, std::string)> partialMatchFP,
+                         std::unordered_set<std::pair<StmtNum, std::string>> ifPatternPairs);
+
+    bool hasExactPattern(StmtNum stmtNum, std::string expr);
+    bool hasPartialPattern(StmtNum stmtNum, std::string expr);
+
+private:
+    std::unordered_map<StmtNum, std::string> ifPatternsMap;
+    void setIfPatterns(const std::unordered_set<std::pair<StmtNum, std::string>>& ifPatternPairs);
+    bool applyIfPatternFunction(std::function<bool(std::string, std::string)> function, StmtNum stmtNum,
+                                std::string expr);
+    std::function<bool(std::string, std::string)> exactMatchFP;
+    std::function<bool(std::string, std::string)> partialMatchFP;
+};
