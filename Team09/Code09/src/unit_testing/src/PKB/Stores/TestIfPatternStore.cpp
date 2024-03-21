35a90617a2a4bb79a39969768c7a8b796c964c0c
Ang Yong Liang <39936106+yl-ang@users.noreply.github.com>
2024-03-21 19:40:00 +0800

    Update TestIfPatternStore.cpp

--- Team09/Code09/src/unit_testing/src/PKB/Stores/TestIfPatternStore.cpp
+++ Team09/Code09/src/unit_testing/src/PKB/Stores/TestIfPatternStore.cpp
@@ -16,50 +16,46 @@ TEST_CASE("IfPatternStore - All Tests") {
     IfPatternStore ifPatternStore;
 
     SECTION("Test always return true for hasExactPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {
-            {1, "(y + 1 > 0)"}, {2, "(x * y * z != 1)"}, {3, "(q + 1 + x + y == 1)"}};
+        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
         std::function<bool(std::string, std::string)> funct_ptr1 = alwaysTrueIfFunction;
         std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseIfFunction;
 
         ifPatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
 
-        std::string arg = "(q + 1 + x + y == 1)";
+        std::string arg = "z";
         REQUIRE(ifPatternStore.hasExactPattern(3, arg));
     }
 
     SECTION("Test always return false for hasExactPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {
-            {1, "(y + 1 > 0)"}, {2, "(x * y * z != 1)"}, {3, "(q + 1 + x + y == 1)"}};
+        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
         std::function<bool(std::string, std::string)> funct_ptr1 = alwaysFalseIfFunction;
         std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseIfFunction;
 
         ifPatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
 
-        std::string arg = "(q + 1 + x + y == 1)";
+        std::string arg = "z";
         REQUIRE_FALSE(ifPatternStore.hasExactPattern(3, arg));
     }
 
     SECTION("Test always return true for hasPartialPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {
-            {1, "(y + 1 > 0)"}, {2, "(x * y * z != 1)"}, {3, "(q + 1 + x + y == 1)"}};
+        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
         std::function<bool(std::string, std::string)> funct_ptr1 = alwaysTrueIfFunction;
         std::function<bool(std::string, std::string)> funct_ptr2 = alwaysTrueIfFunction;
 
         ifPatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
 
-        std::string arg = "(q + 1 + x + y == 1)";
+        std::string arg = "z";
         REQUIRE(ifPatternStore.hasPartialPattern(1, arg));
     }
 
     SECTION("Test always return false for hasPartialPattern - (StmtNum, Expr)") {
-        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {
-            {1, "(y + 1 > 0)"}, {2, "(x * y * z != 1)"}, {3, "(q + 1 + x + y == 1)"}};
+        std::unordered_set<std::pair<StmtNum, std::string>> patterns = {{1, "x"}, {2, "y"}, {3, "z"}, {1, "q"}};
         std::function<bool(std::string, std::string)> funct_ptr1 = alwaysFalseIfFunction;
         std::function<bool(std::string, std::string)> funct_ptr2 = alwaysFalseIfFunction;
 
         ifPatternStore.initialiseStore(funct_ptr1, funct_ptr2, patterns);
 
-        std::string arg = "(q + 1 + x + y == 1)";
+        std::string arg = "z";
         REQUIRE_FALSE(ifPatternStore.hasPartialPattern(1, arg));
     }
 }
