e6ffe3e973eb5f1c78faf41315f27502ae727fef
juslam19 <e0552250@u.nus.edu>
2024-03-23 22:00:13 +0800

    add: unit tests for synonym integer

--- Team09/Code09/src/unit_testing/src/qps/clauses/TestAffectsClause.cpp
+++ Team09/Code09/src/unit_testing/src/qps/clauses/TestAffectsClause.cpp
@@ -199,88 +199,111 @@ TEST_CASE("SuchThatClause evaluate for affects relationship, no synonym") {
     }
 }
 
-// TEST_CASE("SuchThatClause evaluate for affects relationship, one synonym") {
-//     PKB pkb{};
-//     PKBFacadeWriter pfw{pkb};
-//     PKBFacadeReader pfr{pkb};
+TEST_CASE("SuchThatClause evaluate for affects relationship, one synonym") {
+    PKB pkb{};
+    PKBFacadeWriter pfw{pkb};
+    PKBFacadeReader pfr{pkb};
 
-//     SECTION("Affects(synonym, integer)") {
-//         std::unordered_set<Stmt> stmts = {Stmt{StatementType::ASSIGN, 1}, Stmt{StatementType::ASSIGN, 2},
-//                                           Stmt{StatementType::ASSIGN, 3}};
-
-//         std::unordered_set<std::pair<int, int>> nextStoreEntries{std::pair<int, int>{1, 2}, std::pair<int, int>{2, 3}};
-//         pfw.setNextStore(nextStoreEntries);
-
-//         std::unordered_set<std::pair<int, std::string>> usesStoreEntries{std::pair<int, std::string>{2, "x"}, std::pair<int, std::string>{3, "x"}};
-//         pfw.setStatementUsesStore(usesStoreEntries);
-
-//         std::unordered_set<std::pair<int, std::string>> modifiesStoreEntries{std::pair<int, std::string>{1, "x"}, std::pair<int, std::string>{2, "x"}};
-//         pfw.setStatementModifiesStore(modifiesStoreEntries);
-
-//         Synonym* stmtSyn = new Synonym(DesignEntityType::ASSIGN, "s");
-
-//         // Select s such that Affects(s, 2)
-//         AffectsTester{pfr, stmtSyn, new Integer("2")}.testSynonyms({*stmtSyn}).testSynonymValues({{"1"}});
-//         // Select s such that Affects(s, 3)
-//         AffectsTester{pfr, stmtSyn, new Integer("3")}.testSynonyms({*stmtSyn}).testSynonymValues({{"2"}});
-//         // Select s such that Affects(s, 1)
-//         AffectsTester{pfr, stmtSyn, new Integer("1")}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
-
-//         // Select s such that Affects(s, 5), out of bounds
-//         ParentStarTester{pfr, stmtSyn, new Integer("5")}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
-//     }
-
-//     SECTION("Affects(integer, synonym)") {
-//         std::unordered_set<Stmt> stmts = {Stmt{StatementType::ASSIGN, 1}, Stmt{StatementType::ASSIGN, 2},
-//                                           Stmt{StatementType::ASSIGN, 3}};
-
-//         std::unordered_set<std::pair<int, int>> nextStoreEntries{std::pair<int, int>{1, 2}, std::pair<int, int>{2, 3}};
-//         pfw.setNextStore(nextStoreEntries);
-
-//         std::unordered_set<std::pair<int, std::string>> usesStoreEntries{std::pair<int, std::string>{2, "x"}, std::pair<int, std::string>{3, "x"}};
-//         pfw.setStatementUsesStore(usesStoreEntries);
-
-//         std::unordered_set<std::pair<int, std::string>> modifiesStoreEntries{std::pair<int, std::string>{1, "x"}, std::pair<int, std::string>{2, "x"}};
-//         pfw.setStatementModifiesStore(modifiesStoreEntries);
-
-//         Synonym* stmtSyn = new Synonym(DesignEntityType::ASSIGN, "s");
+    SECTION("Affects(synonym, integer)") {
+        std::unordered_set<Variable> variables = {"x", "y"};
         
-//         // Select s such that Affects(2, s)
-//         AffectsTester{pfr, new Integer("2"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{"3"}});
-//         // Select s such that Affects(3, s)
-//         AffectsTester{pfr, new Integer("3"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
-//         // Select s such that Affects(1, s)
-//         AffectsTester{pfr, new Integer("1"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{"2"}});
+        std::unordered_set<Stmt> stmts = {Stmt{StatementType::ASSIGN, 1}, Stmt{StatementType::PRINT, 2},
+                                          Stmt{StatementType::ASSIGN, 3}, Stmt{StatementType::ASSIGN, 4}};
 
-//         // Select s such that Affects(5, s), out of bounds
-//         ParentStarTester{pfr, new Integer("5"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
-//     }
+        /**
+         * x = 1 -- assign/modifies x
+         * print x -- print/uses x
+         * x = x + 1 -- assign/modifies x/ uses x
+         * y = x + 1 -- assign/uses x
+        */
 
-    /**
-     * SECTION("ParentT(Synonym, Wildcard)") {
-        std::unordered_set<Stmt> stmts = {Stmt{StatementType::ASSIGN, 1}, Stmt{StatementType::ASSIGN, 2},
-                                          Stmt{StatementType::ASSIGN, 3}};
+        std::unordered_set<std::pair<int, int>> nextStoreEntries{std::pair<int, int>{1, 2},
+                                                                std::pair<int, int>{2, 3},
+                                                                std::pair<int, int>{3, 4}};
 
-        std::unordered_set<std::pair<int, int>> parentStoreEntries{std::pair<int, int>{1, 2},
-                                                                   std::pair<int, int>{2, 3}};
+        std::unordered_set<std::pair<int, std::string>> usesStoreEntries{std::pair<int, std::string>{2, "x"},
+                                                                        std::pair<int, std::string>{3, "x"},
+                                                                        std::pair<int, std::string>{4, "x"}};
 
+        std::unordered_set<std::pair<int, std::string>> modifiesStoreEntries{std::pair<int, std::string>{1, "x"},
+                                                                            std::pair<int, std::string>{3, "x"},
+                                                                            std::pair<int, std::string>{4, "y"}};
+
+        pfw.setVariables(variables);
         pfw.setStmts(stmts);
-        pfw.setParentStore(parentStoreEntries);
-        Synonym* stmtSyn = new Synonym(DesignEntityType::STMT, "s");
+        pfw.setNextStore(nextStoreEntries);
+        pfw.setStatementUsesStore(usesStoreEntries);
+        pfw.setStatementModifiesStore(modifiesStoreEntries);
 
-        // Select s such that ParentT(s, _)
-        ParentStarTester{pfr, stmtSyn, new Wildcard()}.testSynonyms({*stmtSyn}).testSynonymValues({{"1", "2"}});
+        Synonym* stmtSyn = new Synonym(DesignEntityType::ASSIGN, "s");
+
+        // Select s such that Affects(s, 4)
+        AffectsTester{pfr, stmtSyn, new Integer("4")}.testSynonyms({*stmtSyn}).testSynonymValues({{"3"}});
+        // Select s such that Affects(s, 3)
+        AffectsTester{pfr, stmtSyn, new Integer("3")}.testSynonyms({*stmtSyn}).testSynonymValues({{"1"}});
+        // Select s such that Affects(s, 2)
+        AffectsTester{pfr, stmtSyn, new Integer("2")}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
+        // Select s such that Affects(s, 1)
+        AffectsTester{pfr, stmtSyn, new Integer("1")}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
+
+        // Select s such that Affects(s, 5), out of bounds
+        AffectsTester{pfr, stmtSyn, new Integer("5")}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
     }
-    */
+
+    SECTION("Affects(integer, synonym)") {
+        std::unordered_set<Variable> variables = {"x", "y"};
+        
+        std::unordered_set<Stmt> stmts = {Stmt{StatementType::ASSIGN, 1}, Stmt{StatementType::PRINT, 2},
+                                          Stmt{StatementType::ASSIGN, 3}, Stmt{StatementType::ASSIGN, 4}};
+
+        /**
+         * x = 1 -- assign/modifies x
+         * print x -- print/uses x
+         * x = x + 1 -- assign/modifies x/ uses x
+         * y = x + 1 -- assign/uses x
+        */
+
+        std::unordered_set<std::pair<int, int>> nextStoreEntries{std::pair<int, int>{1, 2},
+                                                                std::pair<int, int>{2, 3},
+                                                                std::pair<int, int>{3, 4}};
+
+        std::unordered_set<std::pair<int, std::string>> usesStoreEntries{std::pair<int, std::string>{2, "x"},
+                                                                        std::pair<int, std::string>{3, "x"},
+                                                                        std::pair<int, std::string>{4, "x"}};
+
+        std::unordered_set<std::pair<int, std::string>> modifiesStoreEntries{std::pair<int, std::string>{1, "x"},
+                                                                            std::pair<int, std::string>{3, "x"},
+                                                                            std::pair<int, std::string>{4, "y"}};
+
+        pfw.setVariables(variables);
+        pfw.setStmts(stmts);
+        pfw.setNextStore(nextStoreEntries);
+        pfw.setStatementUsesStore(usesStoreEntries);
+        pfw.setStatementModifiesStore(modifiesStoreEntries);
+
+        Synonym* stmtSyn = new Synonym(DesignEntityType::ASSIGN, "s");
+
+        // Select s such that Affects(4, s)
+        AffectsTester{pfr, new Integer("4"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
+        // Select s such that Affects(3, s)
+        AffectsTester{pfr, new Integer("3"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{"4"}});
+        // Select s such that Affects(2, s)
+        AffectsTester{pfr, new Integer("2"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
+        // Select s such that Affects(1, s)
+        AffectsTester{pfr, new Integer("1"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{"3"}});
+
+        // Select s such that Affects(s, 5), out of bounds
+        AffectsTester{pfr, new Integer("5"), stmtSyn}.testSynonyms({*stmtSyn}).testSynonymValues({{}});
+    }
 
     // SECTION("Affects(synonym, wildcard)") {
 
     // }
 
-    // SECTION("Affects(synonym, wildcard) / empty store") {
+    // SECTION("Affects(wildcard, synonym)") {
 
     // }
-// }
+}
 
 // TEST_CASE("SuchThatClause evaluate for affects relationship, both synonym") {
 
